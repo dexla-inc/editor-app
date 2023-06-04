748300279520d2fa9f716eaabef5fd7581677357
Reinaldo Schiehll <rn.schiehll@gmail.com>
2023-06-03 23:54:43 -0300

    add value to text modifier

--- components/mapper/Text.tsx
+++ components/mapper/Text.tsx
@@ -1,6 +1,7 @@
+import { useEditorStore } from "@/stores/editor";
 import { Component } from "@/utils/editor";
 import { Text as MantineText, TextProps } from "@mantine/core";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type Props = {
   renderTree: (component: Component) => any;
@@ -8,7 +9,11 @@ type Props = {
 } & TextProps;
 
 export const Text = ({ renderTree, component, ...props }: Props) => {
+  const ref = useRef<HTMLDivElement>();
   const [isEditable, setIsEditable] = useState(false);
+  const updateTreeComponent = useEditorStore(
+    (state) => state.updateTreeComponent
+  );
   const { children, ...componentProps } = component.props as any;
 
   const handleDoubleClick = (e: any) => {
@@ -19,15 +24,20 @@ export const Text = ({ renderTree, component, ...props }: Props) => {
   const handleBlur = (e: any) => {
     e.preventDefault();
     setIsEditable(false);
+    updateTreeComponent(component.id!, {
+      children: ref.current?.innerText,
+    });
   };
 
   return (
     <MantineText
+      ref={ref}
       contentEditable={isEditable}
       onDoubleClick={handleDoubleClick}
       onBlur={handleBlur}
       {...props}
       {...componentProps}
+      suppressContentEditableWarning
     >
       {component.children && component.children.length > 0
         ? component.children?.map((child) => renderTree(child))
--- components/modifiers/Text.tsx
+++ components/modifiers/Text.tsx
@@ -1,13 +1,60 @@
-import { Box, Text } from "@mantine/core";
+import { useEditorStore } from "@/stores/editor";
+import { getComponentById } from "@/utils/editor";
+import { Box, Stack, Text, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 import { IconTextSize } from "@tabler/icons-react";
+import debounce from "lodash.debounce";
+import { useEffect } from "react";
 
 export const icon = IconTextSize;
 export const label = "Text";
 
 export const Modifier = () => {
+  const editorTree = useEditorStore((state) => state.tree);
+  const selectedComponentId = useEditorStore(
+    (state) => state.selectedComponentId
+  );
+  const updateTreeComponent = useEditorStore(
+    (state) => state.updateTreeComponent
+  );
+
+  const debouncedTreeUpdate = debounce(updateTreeComponent, 400);
+
+  const selectedComponent = getComponentById(
+    editorTree.root,
+    selectedComponentId as string
+  );
+
+  const form = useForm({
+    initialValues: {
+      value: "",
+    },
+  });
+
+  useEffect(() => {
+    if (selectedComponent) {
+      const { children = "" } = selectedComponent.props || {};
+      form.setValues({
+        value: children,
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedComponent]);
+
   return (
-    <Box>
-      <Text size="xs">Text Modifier</Text>
-    </Box>
+    <form>
+      <Stack>
+        <TextInput
+          label="Value"
+          {...form.getInputProps("value")}
+          onChange={(e) => {
+            form.setFieldValue("value", e.target.value);
+            debouncedTreeUpdate(selectedComponentId as string, {
+              children: e.target.value,
+            });
+          }}
+        />
+      </Stack>
+    </form>
   );
 };
