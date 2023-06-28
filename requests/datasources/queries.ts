75a7724f28acff3f4e6866fdc12cfccd668e948e
Tom McDonough <tom@dexla.ai>
2023-06-28 18:45:51 +0400

    CU-867863xba - Done authentication step

--- requests/datasources/queries.ts
+++ requests/datasources/queries.ts
@@ -27,3 +27,26 @@ export const getDataSources = async (
 
   return response;
 };
+
+export const getDataSourceEndpoints = async (
+  projectId: string,
+  { type, search, offset, limit }: DataSourcesListParams
+) => {
+  let url = `/projects/${projectId}/datasources`;
+
+  const params = new URLSearchParams();
+
+  if (type) params.append("type", type);
+  if (search) params.append("search", search);
+  if (offset) params.append("offset", offset.toString());
+  if (limit) params.append("limit", limit.toString());
+
+  if (type || search || offset || limit) url += `?${params.toString()}`;
+
+  const response = (await get<PagedResponse<DataSourceResponse>>(
+    url,
+    {}
+  )) as PagedResponse<DataSourceResponse>;
+
+  return response;
+};
