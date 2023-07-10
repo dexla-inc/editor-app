3fd58c4e23e907027e5e517a83050426b96bc288
Tom McDonough <tom@dexla.ai>
2023-07-10 14:45:37 +0400

    Temp fixed typescript errors

--- components/GenerateAIButton.tsx
+++ components/GenerateAIButton.tsx
@@ -1,52 +1,51 @@
 import { getPageEventSource } from "@/requests/pages/queries";
-import { DexlaNotificationProps } from "@/stores/app";
 import { ICON_SIZE } from "@/utils/config";
 import { ActionIcon, Button, Modal, Stack, Textarea } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { EventSourceMessage } from "@microsoft/fetch-event-source";
 
 import { IconSparkles } from "@tabler/icons-react";
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 
 type GenerateAIButtonProps = {
-  projectId?: string;
-  pageTitle?: string;
-  setStream?: Dispatch<SetStateAction<string | undefined>>;
-  startLoading?: (state: DexlaNotificationProps) => void;
-  stopLoading?: (state: DexlaNotificationProps) => void;
+  projectId: string;
+  pageTitle?: string | undefined;
+  // setStream: Dispatch<SetStateAction<string | undefined>>;
+  // startLoading: (state: DexlaNotificationProps) => void;
+  // stopLoading: (state: DexlaNotificationProps) => void;
 };
 
 export const GenerateAIButton = ({
   projectId,
-  pageTitle,
-  setStream,
-  startLoading,
-  stopLoading,
-}: GenerateAIButtonProps) => {
+  pageTitle = "Need to implement",
+}: // setStream,
+// startLoading,
+// stopLoading,
+GenerateAIButtonProps) => {
   const [openedAIModal, { open, close }] = useDisclosure(false);
   const [description, setDescription] = useState("");
 
   const generate = () => {
-    startLoading({
-      id: "page-generation",
-      title: "Generating Page",
-      message: "AI is generating your page",
-    });
+    // startLoading({
+    //   id: "page-generation",
+    //   title: "Generating Page",
+    //   message: "AI is generating your page",
+    // });
 
     const onMessage = (event: EventSourceMessage) => {
       try {
-        setStream((state) => {
-          try {
-            if (state === undefined) {
-              return event.data;
-            } else {
-              return `${state}
-              ${event.data}`;
-            }
-          } catch (error) {
-            return state;
-          }
-        });
+        // setStream((state) => {
+        //   try {
+        //     if (state === undefined) {
+        //       return event.data;
+        //     } else {
+        //       return `${state}
+        //       ${event.data}`;
+        //     }
+        //   } catch (error) {
+        //     return state;
+        //   }
+        // });
       } catch (error) {
         // Do nothing as we expect the stream to not be parsable every time since it can just be halfway through
         console.error(error);
@@ -54,12 +53,12 @@ export const GenerateAIButton = ({
     };
 
     const onError = (err: any) => {
-      stopLoading({
-        id: "page-generation",
-        title: "There was a problem",
-        message: err,
-        isError: true,
-      });
+      // stopLoading({
+      //   id: "page-generation",
+      //   title: "There was a problem",
+      //   message: err,
+      //   isError: true,
+      // });
     };
 
     const onOpen = async (response: Response) => {
@@ -67,11 +66,11 @@ export const GenerateAIButton = ({
     };
 
     const onClose = async () => {
-      stopLoading({
-        id: "page-generation",
-        title: "Page Generated",
-        message: "Here's your page. We hope you like it",
-      });
+      // stopLoading({
+      //   id: "page-generation",
+      //   title: "Page Generated",
+      //   message: "Here's your page. We hope you like it",
+      // });
     };
 
     getPageEventSource(
