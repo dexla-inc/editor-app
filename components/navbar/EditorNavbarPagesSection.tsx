8b85964ba157a4a18ecc8a9cfbe2d3bdf2f40a77
Tom McDonough <tom@dexla.ai>
2023-07-11 19:14:04 +0400

    Reverted page change

--- components/navbar/EditorNavbarPagesSection.tsx
+++ components/navbar/EditorNavbarPagesSection.tsx
@@ -1,38 +1,37 @@
 import { getPageList } from "@/requests/pages/queries";
+import { useEditorStore } from "@/stores/editor";
 import { Stack } from "@mantine/core";
-import { useQuery } from "@tanstack/react-query";
 import debounce from "lodash.debounce";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import InitialPane from "../pages/InitialPane";
 import PageDetailPane from "../pages/PageDetailPane";
 
-type EditorNavbarPagesSectionProps = {
-  isActive: boolean;
-};
-
-export const EditorNavbarPagesSection = ({
-  isActive,
-}: EditorNavbarPagesSectionProps) => {
+export const EditorNavbarPagesSection = () => {
   const router = useRouter();
   const projectId = router.query.id as string;
   const currentPage = router.query.page as string;
   const [showDetail, setShowDetail] = useState<boolean>(false);
   const [search, setSearch] = useState<string>("");
   const debouncedSearch = debounce((query) => setSearch(query), 200);
+  const pages = useEditorStore((state) => state.pages);
+  const setPages = useEditorStore((state) => state.setPages);
 
-  const pages = useQuery({
-    queryKey: ["pages"],
-    queryFn: () => getPageList(projectId, {}),
-    enabled: !!projectId && isActive,
-  });
+  const getPages = useCallback(async () => {
+    const pageList = await getPageList(projectId, { search });
+    setPages(pageList.results);
+  }, [projectId, search, setPages]);
+
+  useEffect(() => {
+    getPages();
+  }, [projectId, getPages]);
 
   return (
     <Stack spacing="xs">
       {!showDetail ? (
         <InitialPane
           setShowDetail={setShowDetail}
-          pages={pages.data?.results || []}
+          pages={pages}
           currentPage={currentPage}
           projectId={projectId}
           debouncedSearch={debouncedSearch}
