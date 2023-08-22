75a58adede4268c6235781935ec7b3107f027126
marcelo-dexla <marcelo@dexla.ai>
2023-08-22 10:34:52 -0300

    removed static styling from navbar

--- components/mapper/Navbar.tsx
+++ components/mapper/Navbar.tsx
@@ -7,26 +7,13 @@ type Props = {
 } & BoxProps;
 
 export const Navbar = ({ renderTree, component, ...props }: Props) => {
-  const { children, style, ...componentProps } = component.props as any;
+  const { children, ...componentProps } = component.props as any;
 
   return (
-    <MantineBox
-      style={{
-        position: "sticky",
-        top: "0",
-        height: "100vh",
-        width: style.width,
-      }}
-    >
-      <MantineBox
-        style={{ ...style, position: "absolute", left: 0, top: 0 }}
-        {...props}
-        {...componentProps}
-      >
-        {component.children && component.children.length > 0
-          ? component.children?.map((child) => renderTree(child))
-          : children}
-      </MantineBox>
+    <MantineBox {...props} {...componentProps}>
+      {component.children && component.children.length > 0
+        ? component.children?.map((child) => renderTree(child))
+        : children}
     </MantineBox>
   );
 };
