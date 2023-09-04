a20a45d03676910bcfe912a8c1034ac97cd52b7b
Tom McDonough <tom@dexla.ai>
2023-09-04 13:50:35 +0100

    CU-862kbrzk8 - Add ability to add collaborators to projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,6 +1,7 @@
 import { DashboardShell } from "@/components/DashboardShell";
 import IconTitleDescriptionButton from "@/components/projects/NewProjectButton";
 import { ProjectItem } from "@/components/projects/ProjectItem";
+import TeamInvitations from "@/components/settings/TeamInvitations";
 import { buttonHoverStyles } from "@/components/styles/buttonHoverStyles";
 import { ProjectResponse, getProjects } from "@/requests/projects/queries";
 import { useAppStore } from "@/stores/app";
@@ -88,7 +89,7 @@ export default function Projects() {
               }}
             />
           )}
-
+          <TeamInvitations />
           {ownedProjects.length > 0 && (
             <>
               {ownedProjects.length > 0 && sharedProjects.length > 0 && (
