e571af395bc5fcaca79dfc6e9ce877e55e1fd921
Tom McDonough <tom@dexla.ai>
2023-09-07 15:34:06 +0100

    Fixed chart not 100% issues

--- components/mapper/charts/Chart.tsx
+++ components/mapper/charts/Chart.tsx
@@ -1,6 +1,6 @@
 import { Component } from "@/utils/editor";
-import { Props as ApexChartsProps } from "react-apexcharts";
 import dynamic from "next/dynamic";
+import { Props as ApexChartsProps } from "react-apexcharts";
 
 const ReactApexChart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
@@ -18,7 +18,8 @@ export const Chart = ({ renderTree, component, ...props }: Props) => {
     <ReactApexChart
       {...props}
       {...componentProps}
-      style={{ ...style, width: "100%" }}
+      style={...style}
+      width="100%"
     >
       {component.children && component.children.length > 0
         ? component.children?.map((child) => renderTree(child))
