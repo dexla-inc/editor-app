ecb489caa2e0d549da26646345649e563af3fc5d
Tom McDonough <tom@dexla.ai>
2023-09-09 06:32:19 +0100

    Revert "Removed console log"
    This reverts commit c64b3819fa8b81ef0513700f32546776b140aa92.

--- components/mapper/Modal.tsx
+++ components/mapper/Modal.tsx
@@ -34,6 +34,7 @@ export const Modal = ({
   } = component.props as any;
 
   const [opened, { open, close }] = useDisclosure(propOpened);
+  console.log({ opened, propOpened });
 
   const handleClose = () => {
     close();
