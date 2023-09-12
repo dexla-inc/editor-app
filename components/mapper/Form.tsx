a07f033a67cf2cde7d333cb15f3874daa5da3ea2
marcelo-dexla <marcelo@dexla.ai>
2023-09-12 12:50:01 -0300

    fix form onSubmit not triggering and radio button not checkable

--- components/mapper/Form.tsx
+++ components/mapper/Form.tsx
@@ -1,18 +1,32 @@
 import { isSame } from "@/utils/componentComparison";
 import { Component } from "@/utils/editor";
 import { FlexProps, Flex as MantineFlex } from "@mantine/core";
-import { memo } from "react";
+import { FormEvent, memo } from "react";
 
 type Props = {
   renderTree: (component: Component) => any;
   component: Component;
+  isPreviewMode: boolean;
 } & FlexProps;
 
 const FormComponent = ({ renderTree, component, ...props }: Props) => {
   const { children, triggers, ...componentProps } = component.props as any;
+  const { onSubmit, ...otherTriggers } = triggers;
+
+  const onSubmitCustom = (e: FormEvent<any>) => {
+    e.preventDefault();
+    return triggers.onSubmit(e);
+  };
 
   return (
-    <MantineFlex {...props} {...componentProps} component="form" {...triggers}>
+    <MantineFlex
+      {...props}
+      {...componentProps}
+      component="form"
+      autoComplete={props.isPreviewMode ? "on" : "off"}
+      onSubmit={onSubmitCustom}
+      {...otherTriggers}
+    >
       {component.children && component.children.length > 0
         ? component.children?.map((child) =>
             renderTree({
