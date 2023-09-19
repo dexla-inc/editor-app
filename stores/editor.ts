101c362df6480cf8c81b5a417ae1e7149f8e33b1
Williams Balogun <williams@dexla.ai>
2023-09-19 16:36:21 +0100

    CU-862kdf6er - Google Places and Places Details API to Google Maps

--- stores/editor.ts
+++ stores/editor.ts
@@ -64,10 +64,6 @@ export type FeatureToBind = {
   index?: number;
 };
 
-export type StoreData = {
-  data?: any;
-};
-
 export type EditorState = {
   tree: EditorTree;
   currentProjectId?: string;
@@ -93,8 +89,6 @@ export type EditorState = {
   copiedAction?: Action[];
   featureToBind?: string;
   featureToBindTo?: FeatureToBind;
-  storeData?: StoreData;
-  setStoreData: (storeData?: StoreData) => void;
   setFeatureToBindTo: (featuretoBindTo?: FeatureToBind) => void;
   setFeatureToBind: (featureToBind?: string) => void;
   setPickingComponentToBindTo: (
@@ -185,7 +179,6 @@ export const useEditorStore = create<EditorState>()(
           set({ pickingComponentToBindFrom }),
         setPickingComponentToBindTo: (pickingComponentToBindTo) =>
           set({ pickingComponentToBindTo }),
-        setStoreData: (storeData) => set({ storeData }),
         setFeatureToBind: (featureToBind) => set({ featureToBind }),
         setFeatureToBindTo: (featureToBindTo) => set({ featureToBindTo }),
         setComponentToBind: (componentToBind) => {
--- utils/actions.ts
+++ utils/actions.ts
@@ -15,6 +15,7 @@ import { OpenToastActionForm } from "@/components/actions/OpenToastActionForm";
 import { PreviousStepActionForm } from "@/components/actions/PreviousStepActionForm";
 import { ReloadComponentActionForm } from "@/components/actions/ReloadComponentActionForm";
 import { TogglePropsActionForm } from "@/components/actions/TogglePropsActionForm";
+import { theme } from "@/pages/_app";
 import {
   getDataSourceAuth,
   getDataSourceEndpoints,
@@ -590,8 +591,6 @@ export const apiCallAction = async ({
   ...rest
 }: APICallActionParams) => {
   const updateTreeComponent = useEditorStore.getState().updateTreeComponent;
-  const setStoreData = useEditorStore.getState().setStoreData;
-  const storeData = useEditorStore.getState().storeData;
 
   try {
     const iframeWindow = useEditorStore.getState().iframeWindow;
@@ -702,7 +701,6 @@ export const apiCallAction = async ({
     }
 
     const responseJson = await response.json();
-    setStoreData({ data: responseJson });
 
     if (onSuccess && onSuccess.sequentialTo === actionId) {
       const actions = component.actions ?? [];
