a4ee704852c330b1b3d1133bb814828ca15ddc06
Williams Balogun <williams@dexla.ai>
2023-09-19 16:36:21 +0100

    CU-862kdf6er - Google Places and Places Details API to Google Maps

--- components/mapper/GoogleMapPlugin.tsx
+++ components/mapper/GoogleMapPlugin.tsx
@@ -33,7 +33,6 @@ const LOADING_TEXT = <Text>Enter API key and refresh the page...</Text>;
 export const GoogleMapPlugin = ({ renderTree, component, ...props }: Props) => {
   const [activeMarkerId, setActiveMarkerId] = useState<string | null>(null);
   const [map, setMap] = useState<any | null>(null);
-  const [forceRender, setForceRender] = useState(false);
 
   const {
     markers,
@@ -97,8 +96,6 @@ export const GoogleMapPlugin = ({ renderTree, component, ...props }: Props) => {
   if (!isLoaded || !apiKey) {
     return LOADING_TEXT;
   }
-
-  console.log(zoom);
 
   return (
     <GoogleMap
