f9aa84c9a47d5853e25e521cacd7b98901c12e3b
marcelo-dexla <marcelo@dexla.ai>
2023-09-20 01:44:15 -0300

    radioItem structure updated

--- components/mapper/structure/Radio.tsx
+++ components/mapper/structure/Radio.tsx
@@ -26,83 +26,7 @@ export const jsonStructure = (props?: any): Component => {
         name: "RadioItem",
         description: "Radio Item",
         props: {
-          value: "radio1",
-          isInsideGroup: true,
-          checked: false,
-          triggers: {},
-        },
-        children: [
-          {
-            id: nanoid(),
-            name: "Container",
-            description: "Container",
-            props: {
-              style: {
-                display: "flex",
-                flexWrap: "wrap",
-                flexDirection: "row",
-                rowGap: "10px",
-                columnGap: "20px",
-                alignItems: "center",
-                justifyContent: "center",
-                position: "relative",
-                padding: "15px",
-                width: "auto",
-                height: "auto",
-                minHeight: "30px",
-                paddingTop: "15px",
-                paddingBottom: "15px",
-                paddingLeft: "15px",
-                paddingRight: "15px",
-              },
-            },
-            children: [
-              {
-                id: nanoid(),
-                name: "Icon",
-                description: "Icon",
-                children: [],
-                props: {
-                  name: "IconCircle",
-                  width: "24px",
-                  style: {
-                    position: "relative",
-                    width: "26px",
-                    height: "26px",
-                  },
-                },
-                blockDroppingChildrenInside: true,
-              },
-              {
-                id: nanoid(),
-                name: "Text",
-                description: "Text",
-                children: [],
-                props: {
-                  children: "Radio 1",
-                  color: "Black.6",
-                  size: "sm",
-                  style: {
-                    lineHeight: "110%",
-                    letterSpacing: "0px",
-                    width: "auto",
-                    height: "auto",
-                    marginRight: "0px",
-                    marginBottom: "0px",
-                  },
-                },
-                blockDroppingChildrenInside: true,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        id: nanoid(),
-        name: "RadioItem",
-        description: "Radio Item",
-        props: {
-          value: "radio2",
+          value: "change-me-1",
           style: {
             borderRadius: px(theme.radius.md),
           },
@@ -149,6 +73,11 @@ export const jsonStructure = (props?: any): Component => {
                   },
                 },
                 blockDroppingChildrenInside: true,
+                states: {
+                  checked: {
+                    name: "IconCircleDot",
+                  },
+                },
               },
               {
                 id: nanoid(),
@@ -156,7 +85,7 @@ export const jsonStructure = (props?: any): Component => {
                 description: "Text",
                 children: [],
                 props: {
-                  children: "Radio 2",
+                  children: "New Radio Item 1",
                   color: "Black.6",
                   size: "sm",
                   style: {
@@ -173,7 +102,90 @@ export const jsonStructure = (props?: any): Component => {
             ],
           },
         ],
-        blockDroppingChildrenInside: true,
+        blockDroppingChildrenInside: false,
+      },
+      {
+        id: nanoid(),
+        name: "RadioItem",
+        description: "Radio Item",
+        props: {
+          value: "change-me-2",
+          style: {
+            borderRadius: px(theme.radius.md),
+          },
+          ...(props.props || {}),
+        },
+        children: [
+          {
+            id: nanoid(),
+            name: "Container",
+            description: "Container",
+            props: {
+              style: {
+                display: "flex",
+                flexWrap: "wrap",
+                flexDirection: "row",
+                rowGap: "10px",
+                columnGap: "20px",
+                alignItems: "center",
+                justifyContent: "center",
+                position: "relative",
+                padding: "15px",
+                width: "auto",
+                height: "auto",
+                minHeight: "30px",
+                paddingTop: "15px",
+                paddingBottom: "15px",
+                paddingLeft: "15px",
+                paddingRight: "15px",
+              },
+            },
+            children: [
+              {
+                id: nanoid(),
+                name: "Icon",
+                description: "Icon",
+                children: [],
+                props: {
+                  name: "IconCircle",
+                  width: "24px",
+                  style: {
+                    position: "relative",
+                    width: "26px",
+                    height: "26px",
+                  },
+                },
+                blockDroppingChildrenInside: true,
+                states: {
+                  checked: {
+                    name: "IconCircleDot",
+                  },
+                },
+              },
+              {
+                id: nanoid(),
+                name: "Text",
+                description: "Text",
+                children: [],
+                props: {
+                  children: "New Radio Item 2",
+                  color: "Black.6",
+                  size: "sm",
+                  style: {
+                    lineHeight: "110%",
+                    letterSpacing: "0px",
+                    width: "auto",
+                    height: "auto",
+                    marginRight: "0px",
+                    marginBottom: "0px",
+                  },
+                },
+                blockDroppingChildrenInside: true,
+              },
+            ],
+          },
+        ],
+        blockDroppingChildrenInside: false,
       },
     ],
   };
