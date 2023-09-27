9d2138fba46c6ac142dd6bf6e015538526e62419
Williams Balogun <williams@dexla.ai>
2023-09-27 15:11:14 +0100

    CU-862ke5hht - Implementation of Component Skeletons During Data Fetching~

--- components/mapper/charts/PieChart.tsx
+++ components/mapper/charts/PieChart.tsx
@@ -1,6 +1,7 @@
 import { Component } from "@/utils/editor";
 import { Props as ApexChartsProps } from "react-apexcharts";
 import { Chart } from "@/components/mapper/charts/Chart";
+import { PieChart as Skeleton } from "../skeleton/Skeleton";
 
 type Props = {
   renderTree: (component: Component) => any;
@@ -8,5 +9,9 @@ type Props = {
 } & ApexChartsProps;
 
 export const PieChart = (props: Props) => {
+  const loading = true;
+  if (loading) {
+    return <Skeleton width="100%" height={500} />;
+  }
   return <Chart {...props} type="pie" />;
 };
