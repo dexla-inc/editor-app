8528e3eea76369afd6ab41572be47c754740d83b
marcelorl-dexla <marcelo@dexla.ai>
2023-10-04 11:54:14 -0300

    added custom javascript flow action form as a logic flow

---
+++ components/actions/logic-flow-forms/CustomJavascriptFlowActionForm.tsx
@@ -0,0 +1,85 @@
+import { ActionButtons } from "@/components/actions/ActionButtons";
+import Editor from "@monaco-editor/react";
+import {
+  handleLoadingStart,
+  handleLoadingStop,
+  updateActionInTree,
+  useActionData,
+  useEditorStores,
+  useLoadingState,
+} from "@/components/actions/_BaseActionFunctions";
+import { CustomJavascriptAction } from "@/utils/actions";
+import { Button, Card, Flex, Stack, Text } from "@mantine/core";
+import { useForm, UseFormReturnType } from "@mantine/form";
+import { IconPlayerPlayFilled } from "@tabler/icons-react";
+import { useState } from "react";
+import { transpile } from "typescript";
+import { ActionsForm } from "@/components/actions/ActionsForm";
+import { useFlowStore } from "@/stores/flow";
+
+type Props = {
+  form: UseFormReturnType<FormValues>;
+};
+
+type FormValues = Omit<CustomJavascriptAction, "name">;
+
+export const CustomJavascriptFlowActionForm = ({ form }: Props) => {
+  const isUpdating = useFlowStore((state) => state.isUpdating);
+  const [codeResult, setCodeResult] = useState<any>();
+
+  const onClickRunCode = () => {
+    let result = transpile(form.values.code);
+    setCodeResult(eval(result));
+  };
+
+  return (
+    <Stack spacing="xs">
+      <Flex w="100%" justify="space-between" align="center">
+        <Text size="sm" weight={500}>
+          Custom Javascript
+        </Text>
+        <Button
+          color="indigo"
+          sx={{ marginRight: 0 }}
+          size="xs"
+          leftIcon={<IconPlayerPlayFilled size={8} />}
+          type="button"
+          onClick={onClickRunCode}
+        >
+          Run Code
+        </Button>
+      </Flex>
+      <Card style={{ overflow: "visible" }} withBorder radius="sm" px={0}>
+        <Editor
+          width="100%"
+          height="150px"
+          defaultLanguage="typescript"
+          defaultValue="// write your code here"
+          {...form.getInputProps("code")}
+          options={{
+            automaticLayout: true,
+            minimap: { enabled: false },
+            contextmenu: false,
+            language: {
+              javascript: {
+                globalKnownSymbols: {
+                  data: true,
+                },
+              },
+            },
+          }}
+        />
+      </Card>
+
+      {codeResult && (
+        <div style={{ height: 200, width: "100%", border: "1px solid #000" }}>
+          {codeResult}
+        </div>
+      )}
+
+      <Button type="submit" size="xs" loading={isUpdating}>
+        Save
+      </Button>
+    </Stack>
+  );
+};
