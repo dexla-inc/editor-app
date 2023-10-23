c065f291f26c3da2b62755eb0188296e7a575118
Williams Balogun <williams@dexla.ai>
2023-10-23 17:39:14 +0100

    CU-862kg804j - Refactor propel auth so we only import @propelauth/react once. Put it in a zustand store

--- pages/projects/[id]/settings/datasources/ai.tsx
+++ pages/projects/[id]/settings/datasources/ai.tsx
@@ -2,18 +2,16 @@ import { DashboardShell } from "@/components/DashboardShell";
 import DataSourceAddAPIWithAI from "@/components/datasources/DataSourceAddAPIWithAI";
 import { SettingsTabHeader } from "@/components/settings/SettingsTabHeader";
 import { SettingsTabs } from "@/components/settings/SettingsTabs";
-import { usePropelAuthStore } from "@/stores/propelAuth";
 import { Tabs } from "@mantine/core";
 import { useRouter } from "next/router";
 
 export default function Settings() {
-  const user = usePropelAuthStore((state) => state.user);
   const router = useRouter();
 
   const { id, name } = router.query as { id: string; name: string };
 
   return (
-    <DashboardShell user={user}>
+    <DashboardShell>
       <SettingsTabHeader name={name} />
       <Tabs defaultValue="datasources" py="xs">
         <SettingsTabs />
--- pages/projects/[id]/settings/datasources/index.tsx
+++ pages/projects/[id]/settings/datasources/index.tsx
@@ -2,17 +2,15 @@ import { DashboardShell } from "@/components/DashboardShell";
 import DataSourceSettings from "@/components/settings/DataSourceSettings";
 import { SettingsTabHeader } from "@/components/settings/SettingsTabHeader";
 import { SettingsTabs } from "@/components/settings/SettingsTabs";
-import { usePropelAuthStore } from "@/stores/propelAuth";
 import { Tabs } from "@mantine/core";
 import { useRouter } from "next/router";
 
 export default function DataSources() {
-  const user = usePropelAuthStore((state) => state.user);
   const router = useRouter();
   const { id, name } = router.query as { id: string; name: string };
 
   return (
-    <DashboardShell user={user}>
+    <DashboardShell>
       <SettingsTabHeader name={name} />
       <Tabs defaultValue="datasources" py="xs">
         <SettingsTabs />
