3ea3e705f75946c0d822d7b9d0c8631a91ae9aab
Williams Balogun <williams@dexla.ai>
2023-10-23 12:09:13 +0100

    CU-862kg804j - Refactor propel auth so we only import @propelauth/react once. Put it in a zustand store

--- stores/propelAuth.ts
+++ stores/propelAuth.ts
@@ -1,6 +1,11 @@
-import { OrgMemberInfo, User } from "@propelauth/react";
-import { UseAuthInfoProps } from "@propelauth/react/dist/types/useAuthInfo";
+import { OrgMemberInfo, User, useLogoutFunction } from "@propelauth/react";
+import {
+  UseAuthInfoProps,
+  useAuthInfo,
+} from "@propelauth/react/dist/types/useAuthInfo";
+import { temporal } from "zundo";
 import { create } from "zustand";
+import { devtools } from "zustand/middleware";
 
 type AuthState = {
   isAuthenticated: boolean;
@@ -9,7 +14,7 @@ type AuthState = {
   organisations?: OrgMemberInfo[];
   role?: any;
   logout: (redirectOnLogout: boolean) => Promise<void>;
-  initializeAuth: (
+  initializeAuth?: (
     authInfo: UseAuthInfoProps,
     logoutFunction: (redirectOnLogout: boolean) => Promise<void>,
   ) => void;
@@ -36,3 +41,38 @@ export const usePropelAuthStore = create<AuthState>((set) => ({
     });
   },
 }));
+
+export const usePropelAuth = create<AuthState>()(
+  devtools(
+    temporal((set) => {
+      // This runs once when the store is initialized
+      if (typeof window !== "undefined") {
+        // Ensuring we're on the client side
+
+        const authInfo = useAuthInfo();
+        const logoutFunction = useLogoutFunction();
+
+        set({
+          user: authInfo.user || ({} as User),
+          organisations: authInfo.orgHelper?.getOrgs(),
+          isAuthenticated: !!authInfo.user,
+          isDexlaAdmin: () => {
+            const org =
+              authInfo.orgHelper?.getOrgByName("Dexla") ||
+              ({} as OrgMemberInfo);
+            return org.userAssignedRole === "DEXLA_ADMIN";
+          },
+          logout: logoutFunction,
+        });
+      }
+
+      return {
+        isAuthenticated: false,
+        user: {} as User,
+        organisations: [],
+        isDexlaAdmin: () => false,
+        logout: async (redirectOnLogout: boolean) => {},
+      };
+    }),
+  ),
+);
