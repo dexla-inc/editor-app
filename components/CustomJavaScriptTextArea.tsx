3753b369aa9232665f1da9a5a90b0a1ba34d6316
marcelorl-dexla <marcelo@dexla.ai>
2023-10-24 20:54:20 -0300

    monaco-editor change variable feature

--- components/CustomJavaScriptTextArea.tsx
+++ components/CustomJavaScriptTextArea.tsx
@@ -1,40 +1,50 @@
-import { Editor } from "@monaco-editor/react";
+import Editor from "@monaco-editor/react";
 
 type JsProps = {
   language: "javascript" | "typescript" | "json";
   value?: string;
+  variables?: Record<string, any>;
+  onChange?: any;
 };
 
 export function CustomJavaScriptTextArea({
   language: defaultLanguage,
   value,
+  variables = {},
+  onChange,
 }: JsProps) {
   // We need to write our own completion providers in JavaScript for Variables, datasources and components.
   // https://www.npmjs.com/package/@monaco-editor/react#for-nextjs-users
-
-  const defaultValue =
-    "// Create a regular JavaScript function that returns\
-      \n// a value and call it after.\
-      \n// This way you can pass in test parameters";
 
   return (
     <Editor
       width="100%"
       height="150px"
-      // if we can get intellisense working with typescript then do it
       defaultLanguage={defaultLanguage ?? "javascript"}
-      value={value ?? defaultValue}
+      onChange={onChange}
+      value={value}
       options={{
         automaticLayout: true,
         minimap: { enabled: false },
         contextmenu: false,
-        language: {
-          javascript: {
-            globalKnownSymbols: {
-              data: true,
-            },
+      }}
+      beforeMount={async (monaco) => {
+        monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+          noSemanticValidation: true,
+          noSyntaxValidation: true,
+        });
+
+        monaco.languages.registerCompletionItemProvider("typescript", {
+          provideCompletionItems: () => {
+            return {
+              suggestions: Object.entries(variables).map(([id, variable]) => ({
+                label: `variables[${variable.name}]`,
+                kind: monaco.languages.CompletionItemKind.Variable,
+                insertText: `variables[/* ${variable.name} */'${id}']`,
+              })),
+            };
           },
-        },
+        });
       }}
     />
   );
