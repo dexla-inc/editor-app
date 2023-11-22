7ca64ff3c7fd410cf1efb421f5e24472687b830d
Williams Balogun <williams@dexla.ai>
2023-11-22 20:10:29 +0100

    fix: changes to the themeColorSelector ux

--- components/OpenThemeButton.tsx
+++ components/OpenThemeButton.tsx
@@ -13,8 +13,8 @@ export const OpenThemeButton = () => {
   return (
     <UnstyledButton
       sx={{
-        ":hover": { textDecoration: "underline", color: theme.colors.gray[9] },
-        color: theme.colors.gray[6],
+        ":hover": { textDecoration: "underline", color: theme.colors.green[9] },
+        color: theme.colors.green[6],
         transition: "all 200ms ease",
       }}
       fz="xs"
