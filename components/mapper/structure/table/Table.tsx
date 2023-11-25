c95ab04e8de02c8d41da61557a62250e3f110258
Williams Balogun <williams@dexla.ai>
2023-11-25 16:25:19 +0100

    splited table into separate components

---
+++ components/mapper/structure/table/Table.tsx
@@ -0,0 +1,158 @@
+import { defaultTheme } from "@/components/IFrame";
+import { tableHeader } from "@/components/mapper/structure/table/TableHeader";
+import { Component } from "@/utils/editor";
+import { nanoid } from "nanoid";
+
+const defaultTableValues = {
+  highlightOnHover: true,
+  horizontalCellSpacing: "sm",
+  verticalCellSpacing: "sm",
+  striped: false,
+  withBorder: false,
+  withColumnBorder: false,
+  style: { width: "100%" },
+};
+
+export const jsonStructure = (props?: any): Component => {
+  const theme = props.theme ?? defaultTheme;
+  const data = props.data ?? [
+    { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
+    { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
+    { position: 39, mass: 88.906, symbol: "Y", name: "Yttrium" },
+    { position: 56, mass: 137.33, symbol: "Ba", name: "Barium" },
+    { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
+    { position: 1, mass: 1.008, symbol: "H", name: "Hydrogen" },
+    { position: 8, mass: 15.999, symbol: "O", name: "Oxygen" },
+    { position: 20, mass: 40.08, symbol: "Ca", name: "Calcium" },
+    { position: 47, mass: 107.87, symbol: "Ag", name: "Silver" },
+    { position: 63, mass: 151.96, symbol: "Eu", name: "Europium" },
+  ];
+
+  return {
+    id: nanoid(),
+    name: "Container",
+    description: "Table",
+    props: {
+      style: {
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        width: "100%",
+      },
+    },
+    blockDroppingChildrenInside: true,
+    children: [
+      tableHeader,
+      {
+        id: nanoid(),
+        name: "Table",
+        description: "Table Body",
+        props: { ...defaultTableValues, ...(props.props || {}) },
+        children: [
+          {
+            id: nanoid(),
+            name: "TableContent",
+            description: "Table Head",
+            blockDroppingChildrenInside: true,
+            props: { type: "thead" },
+            children: [
+              {
+                id: nanoid(),
+                name: "TableRow",
+                description: "Table Row",
+                blockDroppingChildrenInside: true,
+                props: { style: { width: "100%" } },
+                children: [
+                  {
+                    id: nanoid(),
+                    name: "TableCell",
+                    description: "Table Cell",
+                    props: { type: "th" },
+                    children: [
+                      {
+                        id: nanoid(),
+                        name: "Checkbox",
+                        description: "Checkbox",
+                        props: {
+                          style: {
+                            width: "auto",
+                            height: "auto",
+                          },
+                          ...(props.props || {}),
+                        },
+                        blockDroppingChildrenInside: true,
+                      },
+                    ],
+                  },
+                  ...Object.keys(data[0]).map((header) => ({
+                    id: nanoid(),
+                    key: nanoid(),
+                    name: "TableCell",
+                    description: "Table Cell",
+                    props: { type: "th" },
+                    children: [
+                      {
+                        id: nanoid(),
+                        name: "Text",
+                        description: "Text",
+                        props: { children: header },
+                      },
+                    ],
+                  })),
+                ],
+              },
+              ...data.map((item: any) => ({
+                id: nanoid(),
+                key: nanoid(),
+                name: "TableRow",
+                description: "Table Row",
+                blockDroppingChildrenInside: true,
+                props: { style: { width: "100%" } },
+                children: [
+                  {
+                    id: nanoid(),
+                    name: "TableCell",
+                    description: "Table Cell",
+                    props: { type: "td" },
+                    children: [
+                      {
+                        id: nanoid(),
+                        name: "Checkbox",
+                        description: "Checkbox",
+                        props: {
+                          style: {
+                            width: "auto",
+                            height: "auto",
+                          },
+                          ...(props.props || {}),
+                        },
+                        blockDroppingChildrenInside: true,
+                      },
+                    ],
+                  },
+                  ...Object.keys(item).map((data) => ({
+                    id: nanoid(),
+                    key: nanoid(),
+                    name: "TableCell",
+                    description: "Table Cell",
+                    props: { type: "th" },
+                    children: [
+                      {
+                        id: nanoid(),
+                        name: "Text",
+                        description: "Text",
+                        props: {
+                          children: (item as Record<string, any>)[data],
+                        },
+                      },
+                    ],
+                  })),
+                ],
+              })),
+            ],
+          },
+        ],
+      },
+    ],
+  };
+};
---
+++ components/mapper/structure/table/TableHeader.tsx
@@ -0,0 +1,87 @@
+import { defaultButtonValues } from "@/components/modifiers/Button";
+import { defaultInputValues } from "@/components/modifiers/Input";
+import { nanoid } from "nanoid";
+
+export const tableHeader = {
+  id: nanoid(),
+  name: "Container",
+  description: "Table Header",
+  blockDroppingChildrenInside: true,
+  props: {
+    style: {
+      width: "100%",
+      height: "auto",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between",
+      gap: "20px",
+    },
+  },
+  children: [
+    {
+      id: nanoid(),
+      name: "Input",
+      description: "Search Input",
+      props: {
+        ...defaultInputValues,
+        style: { width: "300px", height: "auto" },
+        icon: { props: { name: "IconSearch" } },
+        placeholder: "Search",
+      },
+    },
+    {
+      id: nanoid(),
+      name: "Container",
+      description: "Table Actions",
+      props: {
+        style: {
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          gap: "20px",
+          width: "auto",
+        },
+      },
+      children: [
+        {
+          id: nanoid(),
+          name: "Select",
+          description: "Select",
+          props: {
+            style: {
+              width: "150px",
+            },
+            data: [
+              { label: "Action 1", value: "Action 1" },
+              { label: "Action 2", value: "Action 2" },
+              { label: "Action 3", value: "Action 3" },
+            ],
+            placeholder: "Actions",
+          },
+        },
+        {
+          id: nanoid(),
+          name: "Button",
+          description: "Button",
+          props: {
+            style: {
+              ...defaultButtonValues,
+              width: "auto",
+              height: "auto",
+              padding: "9px",
+              borderRadius: 8,
+              borderColor: "rgba(179, 179, 179, 1)",
+              borderStyle: "solid",
+              borderWidth: "1px",
+            },
+            color: "transparent",
+            textColor: "Black.7",
+            children: "Export CSV",
+            leftIcon: "IconDownload",
+          },
+          blockDroppingChildrenInside: true,
+        },
+      ],
+    },
+  ],
+};
