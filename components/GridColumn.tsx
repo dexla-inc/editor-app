8e4a6001008a0b15c822f78234962a8cf4beba88
Reinaldo Schiehll <rn.schiehll@gmail.com>
2023-11-29 21:44:48 -0300

    improve handler

--- components/GridColumn.tsx
+++ components/GridColumn.tsx
@@ -1,4 +1,4 @@
-import { Box, useMantineTheme } from "@mantine/core";
+import { Box, px, useMantineTheme } from "@mantine/core";
 import { PropsWithChildren, useEffect, useState } from "react";
 import { Resizable } from "re-resizable";
 import { useEditorStore } from "@/stores/editor";
@@ -27,7 +27,6 @@ export const GridColumn = ({
   const [initialWidth, setInitialWidth] = useState(0);
   const [initialSpan, setInitialSpan] = useState(0);
   const [initialNextSiblingSpan, setInitialNextSiblingSpan] = useState(0);
-  const [initialPrevSiblingSpan, setInitialPrevSiblingSpan] = useState(0);
 
   const parent = getComponentParent(editorTree.root, props.id!);
   const siblings = (parent?.children ?? []).filter(
@@ -37,16 +36,20 @@ export const GridColumn = ({
   const compIndex = getComponentIndex(parent!, props.id);
   const nextSibling =
     compIndex < siblings.length - 1 ? siblings[compIndex + 1] : null;
-  // const prevSibling = compIndex > 0 ? siblings[compIndex - 1] : null;
   const isLast = siblings[siblings.length - 1]?.id === props.id;
   const isOnlyChild = siblings.length === 1;
-  const isResizingFromLeft = isLast && !isOnlyChild;
 
   useEffect(() => {
     if (columnSpans[props.id] === undefined) {
       setColumnSpan(props.id, span);
     }
   }, [columnSpans, props.id, setColumnSpan, span]);
+
+  const handleOffset = px(
+    Object.keys(theme.spacing).includes(parent?.props!.gap)
+      ? theme.spacing[parent?.props!.gap ?? "xs"]
+      : parent?.props!.gap ?? theme.spacing.xs,
+  );
 
   return (
     <Box
@@ -64,11 +67,18 @@ export const GridColumn = ({
         top: false,
         right: isLast || isOnlyChild ? false : true,
         bottom: false,
-        left: isResizingFromLeft,
+        left: false,
         topRight: false,
         bottomRight: false,
         bottomLeft: false,
         topLeft: false,
+      }}
+      handleStyles={{
+        right: {
+          width: handleOffset * 4,
+          right: (handleOffset + 20) * -1,
+          zIndex: 90,
+        },
       }}
       onResizeStart={(e: any, direction: any, ref: any, delta: any) => {
         const rect = ref.getBoundingClientRect();
@@ -93,7 +103,6 @@ export const GridColumn = ({
         }
       }}
       onResize={(e: any, direction: any, ref: any, delta: any) => {
-        const isRightResizer = direction === "right";
         const isGoingLeft = delta.width < 0;
         const rect = ref.getBoundingClientRect();
         const newSpan = Math.floor(
@@ -101,31 +110,17 @@ export const GridColumn = ({
         );
         const spanDiff = Math.abs(newSpan - initialSpan);
 
-        if (isRightResizer) {
-          const nextSiblingEl = iframeWindow?.document.getElementById(
-            nextSibling?.id!,
-          );
+        const nextSiblingEl = iframeWindow?.document.getElementById(
+          nextSibling?.id!,
+        );
 
-          if (nextSiblingEl) {
-            const newSiblingSpan = isGoingLeft
-              ? initialNextSiblingSpan + spanDiff
-              : initialNextSiblingSpan - spanDiff;
+        if (nextSiblingEl) {
+          const newSiblingSpan = isGoingLeft
+            ? initialNextSiblingSpan + spanDiff
+            : initialNextSiblingSpan - spanDiff;
 
-            setColumnSpan(nextSibling?.id!, newSiblingSpan);
-          }
-        } /* else {
-          const prevSiblingEl = iframeWindow?.document.getElementById(
-            prevSibling?.props?.id,
-          );
-
-          if (prevSiblingEl) {
-            const newSiblingSpan = isGoingLeft
-              ? initialPrevSiblingSpan - spanDiff
-              : initialPrevSiblingSpan + spanDiff;
-
-            setColumnSpan(prevSibling?.props?.id, newSiblingSpan);
-          }
-        } */
+          setColumnSpan(nextSibling?.id!, newSiblingSpan);
+        }
 
         setColumnSpan(props.id, newSpan);
       }}
@@ -133,31 +128,22 @@ export const GridColumn = ({
         const copy = cloneDeep(editorTree);
         updateTreeComponent(copy.root, props.id, {
           span: columnSpans[props.id] ?? 0,
-          resized: true,
         });
 
-        if (nextSibling && !isResizingFromLeft) {
+        if (nextSibling) {
           updateTreeComponent(copy.root, nextSibling.id!, {
             span: columnSpans[nextSibling.id!] ?? 0,
-            resized: false,
           });
-        } /* else if (prevSibling && resizingFromLeft) {
-          updateTreeComponent(copy.root, prevSibling?.props?.id, {
-            span: columnSpans[prevSibling?.props?.id] ?? 0,
-            resized: false,
-          });
-        } */
 
-        const component = getComponentById(copy.root, props.id!);
-        if (component) {
-          calculateGridSizes(component);
-        }
-
-        if (nextSibling) {
           const nextSiblingComp = getComponentById(copy.root, nextSibling.id!);
           if (nextSiblingComp) {
             calculateGridSizes(nextSiblingComp);
           }
+        }
+
+        const component = getComponentById(copy.root, props.id!);
+        if (component) {
+          calculateGridSizes(component);
         }
 
         setEditorTree(copy);
