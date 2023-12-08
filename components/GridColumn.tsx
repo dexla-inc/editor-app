b8f9faf69a244ba4e67ed94d160b945f658ba075
marcelo-dexla <marcelo@dexla.ai>
2023-12-08 15:29:44 -0300

    tooltip re-implemented back

--- components/GridColumn.tsx
+++ components/GridColumn.tsx
@@ -9,218 +9,223 @@ import { calculateGridSizes } from "@/utils/grid";
 import { Box, Text, px, useMantineTheme } from "@mantine/core";
 import cloneDeep from "lodash.clonedeep";
 import { Resizable } from "re-resizable";
-import { PropsWithChildren, useEffect, useState } from "react";
+import { forwardRef, PropsWithChildren, useEffect, useState } from "react";
 
-export const GridColumn = ({
-  children,
-  style,
-  span,
-  ...props
-}: PropsWithChildren<any>) => {
-  const theme = useMantineTheme();
-  const editorTree = useEditorStore((state) => state.tree);
-  const setEditorTree = useEditorStore((state) => state.setTree);
-  const columnSpans = useEditorStore((state) => state.columnSpans ?? {});
-  const setColumnSpan = useEditorStore((state) => state.setColumnSpan);
-  const iframeWindow = useEditorStore((state) => state.iframeWindow);
-  const setIsResizing = useEditorStore((state) => state.setIsResizing);
-  const [initialWidth, setInitialWidth] = useState(0);
-  const [initialSpan, setInitialSpan] = useState(0);
-  const [initialNextSiblingSpan, setInitialNextSiblingSpan] = useState(0);
+export const GridColumn = forwardRef(
+  ({ children, style, span, ...props }: PropsWithChildren<any>, ref) => {
+    const theme = useMantineTheme();
+    const editorTree = useEditorStore((state) => state.tree);
+    const setEditorTree = useEditorStore((state) => state.setTree);
+    const columnSpans = useEditorStore((state) => state.columnSpans ?? {});
+    const setColumnSpan = useEditorStore((state) => state.setColumnSpan);
+    const iframeWindow = useEditorStore((state) => state.iframeWindow);
+    const setIsResizing = useEditorStore((state) => state.setIsResizing);
+    const [initialWidth, setInitialWidth] = useState(0);
+    const [initialSpan, setInitialSpan] = useState(0);
+    const [initialNextSiblingSpan, setInitialNextSiblingSpan] = useState(0);
 
-  const parent = getComponentParent(editorTree.root, props.id!);
-  const siblings = (parent?.children ?? []).filter(
-    (c) => c.name === "GridColumn",
-  );
+    const parent = getComponentParent(editorTree.root, props.id!);
+    const siblings = (parent?.children ?? []).filter(
+      (c) => c.name === "GridColumn",
+    );
 
-  const compIndex = getComponentIndex(parent!, props.id);
-  const nextSibling =
-    compIndex < siblings.length - 1 ? siblings[compIndex + 1] : null;
-  const isLast = siblings[siblings.length - 1]?.id === props.id;
-  const isOnlyChild = siblings.length === 1;
+    const compIndex = getComponentIndex(parent!, props.id);
+    const nextSibling =
+      compIndex < siblings.length - 1 ? siblings[compIndex + 1] : null;
+    const isLast = siblings[siblings.length - 1]?.id === props.id;
+    const isOnlyChild = siblings.length === 1;
 
-  useEffect(() => {
-    if (columnSpans[props.id] === undefined) {
-      setColumnSpan(props.id, span);
-    }
-  }, [columnSpans, props.id, setColumnSpan, span]);
+    useEffect(() => {
+      if (columnSpans[props.id] === undefined) {
+        setColumnSpan(props.id, span);
+      }
+    }, [columnSpans, props.id, setColumnSpan, span]);
 
-  const handleOffset = px(
-    Object.keys(theme.spacing).includes(parent?.props?.gap)
-      ? theme.spacing[parent?.props?.gap ?? "xs"]
-      : parent?.props?.gap ?? theme.spacing.xs,
-  );
+    const handleOffset = px(
+      Object.keys(theme.spacing).includes(parent?.props?.gap)
+        ? theme.spacing[parent?.props?.gap ?? "xs"]
+        : parent?.props?.gap ?? theme.spacing.xs,
+    );
 
-  return (
-    <>
-      <Box
-        component={Resizable}
-        p="xs"
-        display="grid"
-        style={{
-          gridColumn: `span ${columnSpans[props.id] ?? span}`,
-          gap: theme.spacing.xs,
-          ...(style ?? {}),
-        }}
-        pos="relative"
-        {...props}
-        enable={{
-          top: false,
-          right: isLast || isOnlyChild ? false : true,
-          bottom: false,
-          left: false,
-          topRight: false,
-          bottomRight: false,
-          bottomLeft: false,
-          topLeft: false,
-        }}
-        handleStyles={{
-          right: {
-            width: handleOffset * 4,
-            right: (handleOffset + 20) * -1,
-            zIndex: 90,
-          },
-        }}
-        handleComponent={{
-          right: <Box h={40} id={`handler-${props.id}`} />,
-        }}
-        onResizeStart={(e: any, direction: any, ref: any, delta: any) => {
-          const rect = ref.getBoundingClientRect();
-          const initialSpan = ref.style?.gridColumn.split(" ")[1];
-          setInitialWidth(Math.floor(rect.width));
-          setIsResizing(true);
-          setInitialSpan(parseInt(initialSpan, 10));
+    return (
+      <>
+        <Box
+          ref={ref}
+          component={Resizable}
+          p="xs"
+          display="grid"
+          style={{
+            gridColumn: `span ${columnSpans[props.id] ?? span}`,
+            gap: theme.spacing.xs,
+            ...(style ?? {}),
+          }}
+          pos="relative"
+          {...props}
+          enable={{
+            top: false,
+            right: isLast || isOnlyChild ? false : true,
+            bottom: false,
+            left: false,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          handleStyles={{
+            right: {
+              width: handleOffset * 4,
+              right: (handleOffset + 20) * -1,
+              zIndex: 90,
+            },
+          }}
+          handleComponent={{
+            right: <Box h={40} id={`handler-${props.id}`} />,
+          }}
+          onResizeStart={(e: any, direction: any, ref: any, delta: any) => {
+            const rect = ref.getBoundingClientRect();
+            const initialSpan = ref.style?.gridColumn.split(" ")[1];
+            setInitialWidth(Math.floor(rect.width));
+            setIsResizing(true);
+            setInitialSpan(parseInt(initialSpan, 10));
 
-          if (nextSibling) {
+            if (nextSibling) {
+              const nextSiblingEl = iframeWindow?.document.getElementById(
+                nextSibling?.id!,
+              );
+
+              if (nextSiblingEl) {
+                const initialNextSiblingSpan =
+                  nextSiblingEl.style?.gridColumn.split(" ")[1];
+
+                if (initialNextSiblingSpan) {
+                  setInitialNextSiblingSpan(
+                    parseInt(initialNextSiblingSpan, 10),
+                  );
+                }
+              }
+            }
+          }}
+          onResize={(e: DragEvent, direction: any, ref: any, delta: any) => {
+            const snapIndicator = iframeWindow?.document.getElementById(
+              "column-snap-indicator",
+            );
+            const handler = iframeWindow?.document.getElementById(
+              `handler-${props.id}`,
+            );
+            const snapIndicatorRect = snapIndicator?.getBoundingClientRect();
+            const handlerRect = handler?.getBoundingClientRect();
+
+            const isGoingLeft = delta.width < 0;
+            const rect = ref.getBoundingClientRect();
+            const newSpan = Math.floor(
+              (Math.floor(rect.width) * initialSpan) / initialWidth,
+            );
+            const spanDiff = Math.abs(newSpan - initialSpan);
+
             const nextSiblingEl = iframeWindow?.document.getElementById(
               nextSibling?.id!,
             );
 
             if (nextSiblingEl) {
-              const initialNextSiblingSpan =
-                nextSiblingEl.style?.gridColumn.split(" ")[1];
+              const newSiblingSpan = isGoingLeft
+                ? initialNextSiblingSpan + spanDiff
+                : initialNextSiblingSpan - spanDiff;
 
-              if (initialNextSiblingSpan) {
-                setInitialNextSiblingSpan(parseInt(initialNextSiblingSpan, 10));
+              setColumnSpan(nextSibling?.id!, newSiblingSpan);
+            }
+
+            const isHalfTheParent =
+              newSpan === Math.floor(parent?.props?.gridSize / 2);
+            const isAThirdOfTheParent =
+              newSpan === Math.floor(parent?.props?.gridSize / 3);
+
+            if (isHalfTheParent && handlerRect && snapIndicatorRect) {
+              const txt = snapIndicator?.querySelector("#text");
+              if (txt) {
+                txt.textContent = "1/2";
               }
-            }
-          }
-        }}
-        onResize={(e: DragEvent, direction: any, ref: any, delta: any) => {
-          const snapIndicator = iframeWindow?.document.getElementById(
-            "column-snap-indicator",
-          );
-          const handler = iframeWindow?.document.getElementById(
-            `handler-${props.id}`,
-          );
-          const snapIndicatorRect = snapIndicator?.getBoundingClientRect();
-          const handlerRect = handler?.getBoundingClientRect();
 
-          const isGoingLeft = delta.width < 0;
-          const rect = ref.getBoundingClientRect();
-          const newSpan = Math.floor(
-            (Math.floor(rect.width) * initialSpan) / initialWidth,
-          );
-          const spanDiff = Math.abs(newSpan - initialSpan);
+              snapIndicator!.style.top = `${
+                handlerRect.top - handlerRect.height
+              }px`;
+              snapIndicator!.style.left = `${
+                e.clientX - snapIndicatorRect.width
+              }px`;
+              snapIndicator!.style.display = "block";
+            } else if (
+              isAThirdOfTheParent &&
+              handlerRect &&
+              snapIndicatorRect
+            ) {
+              const txt = snapIndicator?.querySelector("#text");
+              if (txt) {
+                txt.textContent = "1/3";
+              }
 
-          const nextSiblingEl = iframeWindow?.document.getElementById(
-            nextSibling?.id!,
-          );
-
-          if (nextSiblingEl) {
-            const newSiblingSpan = isGoingLeft
-              ? initialNextSiblingSpan + spanDiff
-              : initialNextSiblingSpan - spanDiff;
-
-            setColumnSpan(nextSibling?.id!, newSiblingSpan);
-          }
-
-          const isHalfTheParent =
-            newSpan === Math.floor(parent?.props?.gridSize / 2);
-          const isAThirdOfTheParent =
-            newSpan === Math.floor(parent?.props?.gridSize / 3);
-
-          if (isHalfTheParent && handlerRect && snapIndicatorRect) {
-            const txt = snapIndicator?.querySelector("#text");
-            if (txt) {
-              txt.textContent = "1/2";
+              snapIndicator!.style.top = `${
+                handlerRect.top - handlerRect.height
+              }px`;
+              snapIndicator!.style.left = `${
+                e.clientX - snapIndicatorRect.width
+              }px`;
+              snapIndicator!.style.display = "block";
+            } else {
+              snapIndicator!.style.display = "none";
             }
 
-            snapIndicator!.style.top = `${
-              handlerRect.top - handlerRect.height
-            }px`;
-            snapIndicator!.style.left = `${
-              e.clientX - snapIndicatorRect.width
-            }px`;
-            snapIndicator!.style.display = "block";
-          } else if (isAThirdOfTheParent && handlerRect && snapIndicatorRect) {
-            const txt = snapIndicator?.querySelector("#text");
-            if (txt) {
-              txt.textContent = "1/3";
-            }
-
-            snapIndicator!.style.top = `${
-              handlerRect.top - handlerRect.height
-            }px`;
-            snapIndicator!.style.left = `${
-              e.clientX - snapIndicatorRect.width
-            }px`;
-            snapIndicator!.style.display = "block";
-          } else {
-            snapIndicator!.style.display = "none";
-          }
-
-          setColumnSpan(props.id, newSpan);
-        }}
-        onResizeStop={() => {
-          const copy = cloneDeep(editorTree);
-          updateTreeComponent(copy.root, props.id, {
-            span: columnSpans[props.id] ?? 0,
-            resized: true,
-          });
-
-          if (nextSibling) {
-            updateTreeComponent(copy.root, nextSibling.id!, {
-              span: columnSpans[nextSibling.id!] ?? 0,
-              resized: false,
+            setColumnSpan(props.id, newSpan);
+          }}
+          onResizeStop={() => {
+            const copy = cloneDeep(editorTree);
+            updateTreeComponent(copy.root, props.id, {
+              span: columnSpans[props.id] ?? 0,
+              resized: true,
             });
 
-            const nextSiblingComp = getComponentById(
-              copy.root,
-              nextSibling.id!,
-            );
-            if (nextSiblingComp) {
-              calculateGridSizes(nextSiblingComp);
+            if (nextSibling) {
+              updateTreeComponent(copy.root, nextSibling.id!, {
+                span: columnSpans[nextSibling.id!] ?? 0,
+                resized: false,
+              });
+
+              const nextSiblingComp = getComponentById(
+                copy.root,
+                nextSibling.id!,
+              );
+              if (nextSiblingComp) {
+                calculateGridSizes(nextSiblingComp);
+              }
             }
-          }
 
-          const component = getComponentById(copy.root, props.id!);
-          if (component) {
-            calculateGridSizes(component);
-          }
+            const component = getComponentById(copy.root, props.id!);
+            if (component) {
+              calculateGridSizes(component);
+            }
 
-          setEditorTree(copy);
-          setIsResizing(false);
-        }}
-      >
-        {children}
-      </Box>
-      <Box
-        id="column-snap-indicator"
-        pos="absolute"
-        bg="teal"
-        px="xs"
-        py={0}
-        display="none"
-        sx={{
-          borderRadius: theme.radius.sm,
-          zIndex: 100,
-        }}
-      >
-        <Text id="text" color="white" size="xs">
-          .
-        </Text>
-      </Box>
-    </>
-  );
-};
+            setEditorTree(copy);
+            setIsResizing(false);
+          }}
+        >
+          {children}
+        </Box>
+        <Box
+          id="column-snap-indicator"
+          pos="absolute"
+          bg="teal"
+          px="xs"
+          py={0}
+          display="none"
+          sx={{
+            borderRadius: theme.radius.sm,
+            zIndex: 100,
+          }}
+        >
+          <Text id="text" color="white" size="xs">
+            .
+          </Text>
+        </Box>
+      </>
+    );
+  },
+);
+GridColumn.displayName = "GridColumn";
