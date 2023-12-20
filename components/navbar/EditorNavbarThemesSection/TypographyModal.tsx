20985bd08a1c8afda6eec372afbaf8dfbdc584bd
marcelo-dexla <marcelo@dexla.ai>
2023-12-20 19:24:31 -0300

    replaced typography settings by typography modal

---
+++ components/navbar/EditorNavbarThemesSection/TypographyModal.tsx
@@ -0,0 +1,166 @@
+import {
+  Button,
+  Container,
+  Group,
+  Modal,
+  Select,
+  SelectItem,
+  Stack,
+  Table,
+  Text,
+  Title,
+} from "@mantine/core";
+import { UnitInput } from "@/components/UnitInput";
+import { INPUT_SIZE } from "@/utils/config";
+import { SelectFont } from "@/components/navbar/SelectFont";
+import { useQuery } from "@tanstack/react-query";
+import { getGoogleFonts } from "@/utils/googleFonts";
+import { ThemeResponse } from "@/requests/themes/types";
+import { UseFormReturnType } from "@mantine/form";
+import { fontWeightLabels } from "@/components/navbar/EditorNavbarThemesSection";
+
+type TypographyModalProps = {
+  controls: { opened: boolean; close: () => void };
+  form: UseFormReturnType<ThemeResponse>;
+  onSubmit: () => void;
+  weightsList: Array<SelectItem>;
+};
+
+export const TypographyModal = ({
+  controls,
+  form,
+  onSubmit,
+  weightsList = [],
+}: TypographyModalProps) => {
+  const { data: googleFontsData = [] } = useQuery({
+    queryKey: ["fonts"],
+    queryFn: () => getGoogleFonts(),
+  });
+
+  return (
+    <Modal
+      opened={controls.opened}
+      onClose={controls.close}
+      size="xl"
+      styles={{ overlay: { zIndex: 300 }, inner: { zIndex: 400 } }}
+    >
+      <Container>
+        <form onSubmit={onSubmit}>
+          <Stack spacing="xl">
+            <Title order={2}>Typography Settings</Title>
+
+            <Group>
+              <SelectFont
+                label="Default Font Family"
+                {...form.getInputProps("defaultFont")}
+                onChange={(value: string) => {
+                  form.setFieldValue(`defaultFont`, value);
+                  form.values.fonts.forEach((font, index) => {
+                    if (!font.tag.toLowerCase().startsWith("h")) {
+                      form.setFieldValue(`fonts.${index}.fontFamily`, value);
+                    }
+                  });
+                }}
+              />
+              <SelectFont
+                label="Headings Font Family"
+                value={form.values.fonts[0]?.fontFamily}
+                onChange={(value: string) => {
+                  form.values.fonts.forEach((font, index) => {
+                    if (font.tag.toLowerCase().startsWith("h")) {
+                      form.setFieldValue(`fonts.${index}.fontFamily`, value);
+                    }
+                  });
+                }}
+              />
+            </Group>
+
+            <Table>
+              <thead>
+                <tr>
+                  <th>Style Name</th>
+                  <th>Weight</th>
+                  <th>Size</th>
+                  <th>Line Height</th>
+                  <th>Letter Spacing</th>
+                </tr>
+              </thead>
+              <tbody>
+                {form.values.fonts.map((font, index) => {
+                  const { variants = [] } =
+                    googleFontsData.find(
+                      (f: any) => f.family === font.fontFamily,
+                    ) ?? {};
+
+                  return (
+                    <tr key={index}>
+                      <td style={{ paddingTop: 0, paddingBottom: 0 }}>
+                        <Text
+                          component={font.tag as any}
+                          fz={font.fontSize}
+                          fw={font.fontWeight}
+                          sx={{
+                            fontFamily: font.fontFamily,
+                            lineHeight: font.lineHeight,
+                            letterSpacing: font.letterSpacing,
+                          }}
+                          contentEditable
+                          onInput={(e: any) =>
+                            form.setFieldValue(
+                              `fonts.${index}.tag`,
+                              e.target.textContent,
+                            )
+                          }
+                        >
+                          {font.tag}
+                        </Text>
+                      </td>
+                      <td>
+                        <Select
+                          {...form.getInputProps(`fonts.${index}.fontWeight`)}
+                          data={weightsList}
+                        />
+                      </td>
+                      <td>
+                        <UnitInput
+                          size={INPUT_SIZE}
+                          {...form.getInputProps(`fonts.${index}.fontSize`)}
+                        />
+                      </td>
+                      <td>
+                        <UnitInput
+                          size={INPUT_SIZE}
+                          {...form.getInputProps(`fonts.${index}.lineHeight`)}
+                        />
+                      </td>
+                      <td>
+                        <UnitInput
+                          size={INPUT_SIZE}
+                          {...form.getInputProps(
+                            `fonts.${index}.letterSpacing`,
+                          )}
+                        />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Stack>
+          <Group position="apart" mt={20}>
+            <Button
+              variant="default"
+              type="button"
+              onClick={() => {
+                form.insertListItem("fonts", {});
+              }}
+            >
+              + Add new Font
+            </Button>
+            <Button type="submit">Save</Button>
+          </Group>
+        </form>
+      </Container>
+    </Modal>
+  );
+};
---
+++ components/navbar/SelectFont.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getGoogleFonts } from "@/utils/googleFonts";
+import { Select } from "@mantine/core";
+import { INPUT_SIZE } from "@/utils/config";
+
+type SelectFontProps = {
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+};
+
+export const SelectFont = ({ label, value, onChange }: SelectFontProps) => {
+  const [fontSearch, setFontSearch] = useState("");
+
+  const { data: googleFontsData = [] } = useQuery({
+    queryKey: ["fonts"],
+    queryFn: () => getGoogleFonts(),
+  });
+
+  return (
+    <Select
+      label={label}
+      placeholder="Choose font"
+      value={value}
+      data={googleFontsData
+        .map((f: any) => f.family)
+        .filter((f: string) =>
+          f.toLowerCase().includes(fontSearch.toLowerCase()),
+        )
+        .slice(0, 10)}
+      onChange={onChange}
+      searchable
+      searchValue={fontSearch}
+      onSearchChange={setFontSearch}
+      size={INPUT_SIZE}
+    />
+  );
+};
