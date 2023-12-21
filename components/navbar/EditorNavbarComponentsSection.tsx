e6a84f2e2a6f523e79a51bba86b18368c2334b76
Reinaldo Schiehll <rn.schiehll@gmail.com>
2023-12-20 22:48:28 -0300

    add custom component preview

--- components/navbar/EditorNavbarComponentsSection.tsx
+++ components/navbar/EditorNavbarComponentsSection.tsx
@@ -1,10 +1,12 @@
 import { DraggableComponent } from "@/components/DraggableComponent";
 import { GenerateComponentsAIButton } from "@/components/GenerateComponentsAIButton";
+import { useUserTheme } from "@/hooks/useUserTheme";
 import { getComponentList } from "@/requests/components/queries";
 import { CustomComponentResponse } from "@/requests/components/types";
 import { usePropelAuthStore } from "@/stores/propelAuth";
 import {
   ComponentCategoryType,
+  componentMapper,
   structureMapper,
 } from "@/utils/componentMapper";
 import { decodeSchema } from "@/utils/compression";
@@ -13,6 +15,8 @@ import { toSpaced } from "@/utils/dashboardTypes";
 import {
   Center,
   Grid,
+  HoverCard,
+  MantineProvider,
   SegmentedControl,
   Stack,
   Text,
@@ -22,7 +26,9 @@ import {
 import { IconFrustum, IconSearch } from "@tabler/icons-react";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import createCache from "@emotion/cache";
+import { Component } from "@/utils/editor";
 
 type DraggableComponentData = {
   id: string;
@@ -36,6 +42,8 @@ export const EditorNavbarComponentsSection = () => {
     useState<string>("default");
   const router = useRouter();
   const activeCompany = usePropelAuthStore((state) => state.activeCompany);
+  const customStackRef = useRef<HTMLDivElement>(null);
+  const userTheme = useUserTheme(router.query.id! as string);
 
   const componentList = useQuery({
     queryKey: ["components"],
@@ -63,6 +71,12 @@ export const EditorNavbarComponentsSection = () => {
 
   const customComponents =
     componentList.data?.results.filter((c) => c.scope !== "GLOBAL") ?? [];
+
+  const renderTree = useCallback((component: Component) => {
+    const componentToRender = componentMapper[component.name];
+
+    return componentToRender?.Component({ component, renderTree });
+  }, []);
 
   return (
     <Stack spacing="xl">
@@ -120,7 +134,7 @@ export const EditorNavbarComponentsSection = () => {
       )}
 
       {componentTypeToShow === "custom" && (
-        <Stack spacing="xs">
+        <Stack spacing="xs" ref={customStackRef}>
           {customComponents?.length === 0 && (
             <Center>
               <Text align="center" size="xs" color="dimmed">
@@ -129,27 +143,56 @@ export const EditorNavbarComponentsSection = () => {
               </Text>
             </Center>
           )}
-          <Grid gutter="xs">
-            {(query
-              ? customComponents.filter((cc) =>
-                  new RegExp(query, "i").test(cc.description),
-                )
-              : customComponents
-            ).map(
-              ({ id, content, description, type }: CustomComponentResponse) => {
-                return (
-                  <Grid.Col span={6} key={id}>
-                    <DraggableComponent
-                      icon={<IconFrustum size={LARGE_ICON_SIZE} />}
-                      id={id}
-                      text={description}
-                      data={JSON.parse(decodeSchema(content))}
-                    />
-                  </Grid.Col>
-                );
-              },
-            )}
-          </Grid>
+          <MantineProvider
+            withNormalizeCSS
+            theme={userTheme}
+            emotionCache={createCache({
+              container: customStackRef.current ?? undefined,
+              key: "custom-components",
+            })}
+          >
+            <Grid gutter="xs">
+              {(query
+                ? customComponents.filter((cc) =>
+                    new RegExp(query, "i").test(cc.description),
+                  )
+                : customComponents
+              ).map(
+                ({
+                  id,
+                  content,
+                  description,
+                  type,
+                }: CustomComponentResponse) => {
+                  const componentData = JSON.parse(decodeSchema(content));
+
+                  return (
+                    <HoverCard
+                      key={id}
+                      width="auto"
+                      shadow="md"
+                      withinPortal
+                      position="right"
+                    >
+                      <HoverCard.Target>
+                        <Grid.Col span={6}>
+                          <DraggableComponent
+                            icon={<IconFrustum size={LARGE_ICON_SIZE} />}
+                            id={id}
+                            text={description}
+                            data={componentData}
+                          />
+                        </Grid.Col>
+                      </HoverCard.Target>
+                      <HoverCard.Dropdown sx={{ pointerEvents: "none" }}>
+                        {renderTree(componentData)}
+                      </HoverCard.Dropdown>
+                    </HoverCard>
+                  );
+                },
+              )}
+            </Grid>
+          </MantineProvider>
         </Stack>
       )}
     </Stack>
