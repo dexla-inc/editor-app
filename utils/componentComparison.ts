a493f5dc9f053b46159613004e160e425b56b230
Reinaldo Schiehll <rn.schiehll@gmail.com>
2023-12-26 21:57:58 -0300

    fix select

--- utils/componentComparison.ts
+++ utils/componentComparison.ts
@@ -1,6 +1,3 @@
 export const isSame = (prevProps: any, nextProps: any) => {
-  const { triggers: prevTriggers, ...prev } = prevProps?.component?.props;
-  const { triggers: nextTriggers, ...next } = nextProps?.component?.props;
-
   return false;
 };
