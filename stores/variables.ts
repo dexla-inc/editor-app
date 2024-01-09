6d88b08f3e300fd1772b0e45d9a7eb73c59dfd8a
Williams Balogun <141737816+iHemz@users.noreply.github.com>
2024-01-09 15:18:03 +0100

    persist variable store (#505)
    * persist variable store
    
    * persist variable store

--- stores/variables.ts
+++ stores/variables.ts
@@ -1,41 +1,51 @@
 import { VariableParams, VariableResponse } from "@/requests/variables/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type VariableList = Array<VariableResponse>;
 
 type VariablesState = {
-  [key: string]: any;
   variableList: VariableList;
   initializeVariableList: (response: any) => void;
   setVariable: (variable: VariableParams, id: string) => void;
   deleteVariable: (variableId: string) => void;
 };
 
-export const useVariableStore = create<VariablesState>((set, get) => ({
-  variableList: [],
-  initializeVariableList: (response) => {
-    const variableList = response?.results?.map(
-      (variable: VariableResponse) => variable,
-    );
-    set({ variableList });
-  },
-  setVariable: (variable, id) => {
-    const _variableList = get().variableList;
-    const index = _variableList.findIndex((item) => item?.id === id);
-    if (index >= 0) {
-      _variableList[index] = { id, ...variable };
-      set({ variableList: [..._variableList] });
-    }
-    set({
-      variableList: [..._variableList, { id, ...variable }],
-    });
-  },
-  deleteVariable: (variableId) => {
-    const _variableList = get().variableList;
-    const index = _variableList.findIndex((item) => item?.id === variableId);
-    if (index >= 0) {
-      _variableList.splice(index, 1);
-      set({ variableList: [..._variableList] });
-    }
-  },
-}));
+export const useVariableStore = create<VariablesState>()(
+  persist(
+    (set, get) => ({
+      variableList: [] as VariableList,
+      initializeVariableList: (response) => {
+        const variableList = (response?.results || []) as VariableList;
+        set({ variableList });
+      },
+      setVariable: (variable, id) => {
+        const _variableList = get().variableList;
+        const index = _variableList.findIndex((item) => item?.id === id);
+        if (index >= 0) {
+          _variableList[index] = { id, ...variable };
+          set({ variableList: [..._variableList] });
+        }
+        set({
+          variableList: [..._variableList, { id, ...variable }],
+        });
+      },
+      deleteVariable: (variableId) => {
+        const _variableList = get().variableList;
+        const index = _variableList.findIndex(
+          (item) => item?.id === variableId,
+        );
+        if (index >= 0) {
+          _variableList.splice(index, 1);
+          set({ variableList: [..._variableList] });
+        }
+      },
+    }),
+    {
+      name: "variables-storage",
+      partialize: (state: VariablesState) => ({
+        variableList: state.variableList,
+      }),
+    },
+  ),
+);
