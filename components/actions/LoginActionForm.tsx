65521a681f18cb64db650fcfa0cdc3d7405f75ac
Tom McDonough <tom@dexla.ai>
2024-01-11 11:16:48 +0000

    Fix ts errors

--- components/actions/LoginActionForm.tsx
+++
@@ -1,9 +0,0 @@
-import { APICallActionForm } from "@/components/actions/APICallActionForm";
-
-type Props = {
-  id: string;
-};
-
-export const LoginActionForm = (props: Props) => {
-  return <APICallActionForm {...props} actionName="login" />;
-};
