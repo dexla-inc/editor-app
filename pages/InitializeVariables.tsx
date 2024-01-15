6f8dde1761aa48abf69373d3e110cea4cfc44f42
Williams Balogun <williams@dexla.ai>
2024-01-15 22:05:37 +0100

    fix variables lost in deployment

---
+++ pages/InitializeVariables.tsx
@@ -0,0 +1,30 @@
+import { useVariableListQuery } from "@/hooks/reactQuery/useVariableListQuery";
+import { useEditorStore } from "@/stores/editor";
+import { useVariableStore } from "@/stores/variables";
+import { useEffect } from "react";
+
+type Props = {
+  isLive: boolean;
+  pageProps: any;
+};
+
+export const InitializeVariables = ({ isLive, pageProps }: Props) => {
+  const projectId = useEditorStore((state) => state.currentProjectId);
+  const pageId = useEditorStore((state) => state.currentPageId);
+  const _projectId = pageProps.id || projectId;
+  const _pageId = pageProps.page?.id || pageId;
+  const initializeVariableList = useVariableStore(
+    (state) => state.initializeVariableList,
+  );
+
+  const { data: variables, isLoading: isVariablesFetching } =
+    useVariableListQuery(_projectId);
+
+  useEffect(() => {
+    if (!isVariablesFetching && variables?.results)
+      initializeVariableList(variables?.results);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [_pageId, variables]);
+
+  return null;
+};
