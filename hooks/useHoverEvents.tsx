25e66cda2a8f639ba45eff0e2b9d00e5e0e22744
Tom McDonough <71598973+tjmcdonough@users.noreply.github.com>
2024-01-22 16:18:11 +0000

    Remove hover events (#574)
    * Removed on hover events as it was re re-rendering components
    
    * Removed on hover events as it was re re-rendering components
    
    * Added margin and padding back in
    
    * Changed onHoverState hook to onHoverEvents
    
    * Changed onHoverState hook to onHoverEvents

---
+++ hooks/useHoverEvents.tsx
@@ -0,0 +1,30 @@
+export const useHoverState = (
+  setHoveredComponentId: (hoveredComponentId?: string | undefined) => void,
+  updateOverlays: any,
+  hoveredComponentId: any,
+  iframeWindow: any,
+) => {
+  const handleMouseEnter = (e: any, id?: string) => {
+    e.stopPropagation();
+    const newHoveredId = e.currentTarget.id;
+    setHoveredComponentId(newHoveredId);
+    const element = (iframeWindow ?? window).document.getElementById(
+      id ?? newHoveredId,
+    );
+    updateOverlays(element);
+  };
+
+  const handleMouseLeave = (e: any) => {
+    e.stopPropagation(); // Stop the event from bubbling up to prevent child's onMouseLeave affecting parent
+    setTimeout(() => {
+      if (hoveredComponentId === e.currentTarget?.id) {
+        setHoveredComponentId("");
+      }
+    }, 10);
+  };
+
+  return {
+    handleMouseEnter,
+    handleMouseLeave,
+  };
+};
