87ce9fd379e12424f497b5361bf2c858e0fcb261
marcelo-dexla <marcelo@dexla.ai>
2024-01-22 12:32:36 -0300

    removed debouncing when updating attrs

--- components/data/Data.tsx
+++ components/data/Data.tsx
@@ -1,7 +1,6 @@
 import { SegmentedControl, Stack } from "@mantine/core";
-import { Component, debouncedTreeComponentAttrsUpdate } from "@/utils/editor";
+import { Component } from "@/utils/editor";
 import { dataMapper } from "@/utils/dataMapper";
-import { ReactNode } from "react";
 import { useEditorStore } from "@/stores/editor";
 import { useDataSourceEndpoints } from "@/hooks/reactQuery/useDataSourceEndpoints";
 
@@ -11,6 +10,9 @@ type DataProps = {
 export const Data = ({ component }: DataProps) => {
   const projectId = useEditorStore((state) => state.currentProjectId);
   const { data: endpoints } = useDataSourceEndpoints(projectId);
+  const updateTreeComponentAttrs = useEditorStore(
+    (state) => state.updateTreeComponentAttrs,
+  );
 
   const DataSection = dataMapper[component?.name as keyof typeof dataMapper];
 
@@ -23,12 +25,13 @@ export const Data = ({ component }: DataProps) => {
       <SegmentedControl
         w="100%"
         size="xs"
+        value={component.props?.dataType}
         data={[
           { label: "Static", value: "static" },
           { label: "Dynamic", value: "dynamic" },
         ]}
         onChange={(e) =>
-          debouncedTreeComponentAttrsUpdate({ props: { dataType: e } })
+          updateTreeComponentAttrs([component.id!], { props: { dataType: e } })
         }
       />
       <DataSection
