a4fed0adcfe7accd2342f73955cdc97531ac931d
Tom McDonough <tom@dexla.ai>
2024-02-02 08:51:47 -0800

    Fixed error when clicking actions

--- components/ComponentToBindFromInput.tsx
+++ components/ComponentToBindFromInput.tsx
@@ -1,8 +1,14 @@
 import BindingPopover from "@/components/BindingPopover";
 import { useEditorStore } from "@/stores/editor";
-import { Flex, TextInput, TextInputProps } from "@mantine/core";
 import { AUTOCOMPLETE_OFF_PROPS } from "@/utils/common";
 import { ValueProps } from "@/utils/types";
+import {
+  Flex,
+  MantineTheme,
+  TextInput,
+  TextInputProps,
+  useMantineTheme,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
 type Props = Omit<TextInputProps, "value" | "onChange"> & {
@@ -23,9 +29,6 @@ export const ComponentToBindFromInput = ({
   onChange,
   ...props
 }: Props) => {
-  const setPickingComponentToBindTo = useEditorStore(
-    (state) => state.setPickingComponentToBindTo,
-  );
   const setHighlightedComponentId = useEditorStore(
     (state) => state.setHighlightedComponentId,
   );
@@ -34,26 +37,17 @@ export const ComponentToBindFromInput = ({
     { open: onOpenBindingPopOver, close: onCloseBindingPopOver },
   ] = useDisclosure(false);
 
-  const onBindComponent = () => {
-    setPickingComponentToBindTo({
-      componentId: componentId || "",
-      onPick: onPickComponent,
-    });
-  };
+  const theme = useMantineTheme();
+  const styles = useTextInputStyles(theme);
 
   return (
     <Flex align="end" gap="xs" pos="relative">
-      {value.dataType === "boundCode" && (
+      {/* TODO: This value should never be empty  */}
+      {value?.dataType === "boundCode" && (
         <TextInput
           pos="absolute"
           w="100%"
-          styles={{
-            root: { zIndex: 100 },
-            input: {
-              cursor: "pointer",
-              "&:disabled": { opacity: 1 },
-            },
-          }}
+          styles={styles}
           readOnly
           value="< Edit Code >"
           disabled={isBindingPopOverOpen}
@@ -107,3 +101,18 @@ export const ComponentToBindFromInput = ({
     </Flex>
   );
 };
+
+const useTextInputStyles = (theme: MantineTheme) => ({
+  root: { zIndex: 100 },
+  input: {
+    cursor: "pointer",
+    backgroundColor: theme.colors.teal[4],
+    color: theme.black,
+    "&:disabled": {
+      opacity: 1,
+      color: theme.black,
+      backgroundColor: theme.colors.teal[5],
+    },
+    "&:hover": { backgroundColor: theme.colors.teal[5] },
+  },
+});
