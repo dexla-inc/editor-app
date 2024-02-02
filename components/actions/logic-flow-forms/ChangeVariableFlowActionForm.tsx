a9700d0278ef9b694fe57198c8469f1f2c6bbba4
marcelo-dexla <marcelo@dexla.ai>
2024-02-02 13:16:50 -0300

    fixed bindingPopover usage

--- components/actions/logic-flow-forms/ChangeVariableFlowActionForm.tsx
+++ components/actions/logic-flow-forms/ChangeVariableFlowActionForm.tsx
@@ -4,6 +4,7 @@ import { useFlowStore } from "@/stores/flow";
 import { ChangeVariableAction } from "@/utils/actions";
 import { Button, Stack } from "@mantine/core";
 import { UseFormReturnType } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
 
 type Props = {
   form: UseFormReturnType<FormValues>;
@@ -13,12 +14,24 @@ type FormValues = Omit<ChangeVariableAction, "name">;
 
 export const ChangeVariableFlowActionForm = ({ form }: Props) => {
   const isUpdating = useFlowStore((state) => state.isUpdating);
+  const [
+    isBindingPopOverOpen,
+    { open: onOpenBindingPopOver, close: onCloseBindingPopOver },
+  ] = useDisclosure(false);
 
   return (
     <Stack spacing="xs">
       <VariableSelect required {...form.getInputProps("variableId")} />
 
-      <BindingPopover style="iconButton" {...form.getInputProps("value")} />
+      <BindingPopover
+        controls={{
+          isOpen: isBindingPopOverOpen,
+          onClose: onCloseBindingPopOver,
+          onOpen: onOpenBindingPopOver,
+        }}
+        style="iconButton"
+        {...form.getInputProps("value")}
+      />
 
       <Button type="submit" size="xs" loading={isUpdating}>
         Save
