2ede4b74c341b00a46d2fc6252497810100c0355
Tom McDonough <tom@dexla.ai>
2024-02-05 08:21:33 -0800

    Fixed intermittent issue when going to editor. Was calling every page from every project

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -1,72 +1,46 @@
-import { Icon } from "@/components/Icon";
-import { usePageListQuery } from "@/hooks/reactQuery/usePageListQuery";
-import { PageResponse } from "@/requests/pages/types";
-import { deleteProject } from "@/requests/projects/mutations";
+import { buttonHoverStyles } from "@/components/styles/buttonHoverStyles";
+import { getPageList } from "@/requests/pages/queries-noauth";
 import { ProjectResponse } from "@/requests/projects/types";
 import { usePropelAuthStore } from "@/stores/propelAuth";
 import { THIN_DARK_OUTLINE, THIN_GRAY_OUTLINE } from "@/utils/branding";
-import { ICON_DELETE, ICON_SIZE, LARGE_ICON_SIZE } from "@/utils/config";
 import { regionTypeFlags } from "@/utils/dashboardTypes";
 import {
   Avatar,
   Box,
   Col,
-  Collapse,
   Flex,
-  Loader,
-  MantineTheme,
-  Menu,
-  NavLink,
-  Skeleton,
   Text,
   UnstyledButton,
+  useMantineTheme,
 } from "@mantine/core";
-import {
-  IconChevronDown,
-  IconChevronRight,
-  IconDots,
-  IconFileAnalytics,
-  IconHome,
-  IconSettings,
-} from "@tabler/icons-react";
-import Link from "next/link";
+import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { ProjectItemMenu } from "./ProjectItemMenu";
 
 type ProjectItemProps = {
   project: ProjectResponse;
-  theme: MantineTheme;
-  buttonHoverStyles: any;
   goToEditor: (projectId: string, pageId: string) => Promise<void>;
   onDeleteProject?: (id: string) => void;
 };
 
 export function ProjectItem({
   project,
-  theme,
-  buttonHoverStyles,
   goToEditor,
   onDeleteProject,
 }: ProjectItemProps) {
   const router = useRouter();
+  const theme = useMantineTheme();
   const [isHovered, setIsHovered] = useState(false);
-  const [pages, setPages] = useState<PageResponse[]>([]);
-  const [pagesLoading] = useState(false);
-  const [opened, setOpened] = useState(false);
-  const [settingsOpened, setSettingsOpened] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const { isDexlaAdmin, company } = usePropelAuthStore((state) => ({
-    isDexlaAdmin: state.isDexlaAdmin,
-    company: state.activeCompany,
-  }));
-
-  const { data: pageListQuery, invalidate } = usePageListQuery(project.id);
-
-  useEffect(() => {
-    setPages(pageListQuery?.results || []);
-  }, [pageListQuery]);
+  const company = usePropelAuthStore((state) => state.activeCompany);
+  const queryClient = useQueryClient();
 
   const goToEditorHomePage = async () => {
+    const data = await queryClient.fetchQuery(["pages", project.id], () =>
+      getPageList(project.id),
+    );
+    const pages = data.results;
+
     const homePage = pages.find((page) => page.isHome);
     let pageId: string | undefined = homePage?.id;
 
@@ -80,13 +54,6 @@ export function ProjectItem({
       const newProjectUrl = `/projects/new?company=${company.orgId}&projectId=${project.id}&step=2`;
       router.push(newProjectUrl);
     }
-  };
-
-  const deleteProjectFn = async () => {
-    setIsLoading(true);
-    await deleteProject(project.id);
-    onDeleteProject && onDeleteProject(project.id);
-    setPages([]);
   };
 
   const isDarkTheme = theme.colorScheme === "dark";
@@ -133,121 +100,12 @@ export function ProjectItem({
             </Text>
             <Avatar src={regionTypeFlags[project.region.type]} size="sm" />
           </Flex>
-          <Menu width={250} withArrow offset={20} onOpen={invalidate}>
-            <Menu.Target>
-              <UnstyledButton
-                sx={{
-                  borderRadius: theme.radius.sm,
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  ...buttonHoverStyles(theme),
-                }}
-              >
-                {isLoading ? (
-                  <Loader size="sm" />
-                ) : (
-                  <IconDots
-                    size={LARGE_ICON_SIZE}
-                    color={theme.colors.teal[5]}
-                  />
-                )}
-              </UnstyledButton>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Label>Project</Menu.Label>
-              <NavLink
-                label="Pages"
-                icon={<IconFileAnalytics size={ICON_SIZE} />}
-                rightSection={
-                  opened ? (
-                    <IconChevronDown size={ICON_SIZE} />
-                  ) : (
-                    <IconChevronRight size={ICON_SIZE} />
-                  )
-                }
-                onClick={() => setOpened((isOpen) => !isOpen)}
-              />
-              <Collapse in={opened}>
-                {pages.map((page) => {
-                  return (
-                    <Skeleton key={page.id} visible={pagesLoading}>
-                      <Box ml={10}>
-                        <Menu.Item
-                          icon={
-                            page.isHome ? (
-                              <IconHome size={ICON_SIZE} />
-                            ) : (
-                              <IconFileAnalytics size={ICON_SIZE} />
-                            )
-                          }
-                          onClick={() => goToEditor(project.id, page.id)}
-                        >
-                          {page.title}
-                        </Menu.Item>
-                      </Box>
-                    </Skeleton>
-                  );
-                })}
-              </Collapse>
-              <NavLink
-                label="Settings"
-                icon={<IconSettings size={ICON_SIZE} />}
-                rightSection={
-                  settingsOpened ? (
-                    <IconChevronDown size={ICON_SIZE} />
-                  ) : (
-                    <IconChevronRight size={ICON_SIZE} />
-                  )
-                }
-                onClick={() => setSettingsOpened((isOpen) => !isOpen)}
-              />
-              <Collapse in={settingsOpened}>
-                <Box ml={10}>
-                  <Menu.Item
-                    icon={<Icon name="IconSettings2" size={ICON_SIZE} />}
-                    component={Link}
-                    href={`/projects/${project.id}/settings?name=${project.friendlyName}`}
-                  >
-                    General
-                  </Menu.Item>
-                  <Menu.Item
-                    icon={<Icon name="IconDatabase" size={ICON_SIZE} />}
-                    component={Link}
-                    href={`/projects/${project.id}/settings/datasources?name=${project.friendlyName}`}
-                  >
-                    Datasource
-                  </Menu.Item>
-                  <Menu.Item
-                    icon={<Icon name="IconWorldWww" size={ICON_SIZE} />}
-                    component={Link}
-                    href={`/projects/${project.id}/settings/domain?name=${project.friendlyName}`}
-                  >
-                    Domain
-                  </Menu.Item>
-                </Box>
-              </Collapse>
-              {isDexlaAdmin && (
-                <Menu.Item
-                  icon={<Icon name="IconRefresh" />}
-                  color={isDarkTheme ? "teal" : "indigo"}
-                  component={Link}
-                  href={`/projects/new?company=${company.orgId}&projectId=${project.id}&step=2`}
-                >
-                  Regenerate Pages
-                </Menu.Item>
-              )}
-              {onDeleteProject && (
-                <Menu.Item
-                  icon={<Icon name={ICON_DELETE} />}
-                  color="red"
-                  onClick={deleteProjectFn}
-                >
-                  Delete
-                </Menu.Item>
-              )}
-            </Menu.Dropdown>
-          </Menu>
+          <ProjectItemMenu
+            projectId={project.id}
+            projectFriendlyName={project.friendlyName}
+            goToEditor={goToEditor}
+            onDeleteProject={onDeleteProject}
+          />
         </Flex>
       </Box>
     </Col>
---
+++ components/projects/ProjectItemMenu.tsx
@@ -0,0 +1,203 @@
+import { Icon } from "@/components/Icon";
+import { buttonHoverStyles } from "@/components/styles/buttonHoverStyles";
+import { usePageListQuery } from "@/hooks/reactQuery/usePageListQuery";
+import { PageResponse } from "@/requests/pages/types";
+import { deleteProject } from "@/requests/projects/mutations";
+import { usePropelAuthStore } from "@/stores/propelAuth";
+import { ICON_DELETE, ICON_SIZE, LARGE_ICON_SIZE } from "@/utils/config";
+import {
+  Box,
+  Collapse,
+  Loader,
+  Menu,
+  NavLink,
+  Skeleton,
+  UnstyledButton,
+  useMantineTheme,
+} from "@mantine/core";
+import {
+  IconChevronDown,
+  IconChevronRight,
+  IconDots,
+  IconFileAnalytics,
+  IconHome,
+  IconSettings,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Props = {
+  projectId: string;
+  projectFriendlyName: string;
+  goToEditor: (projectId: string, pageId: string) => Promise<void>;
+  onDeleteProject?: (id: string) => void;
+};
+
+export function ProjectItemMenu({
+  projectId,
+  projectFriendlyName,
+  goToEditor,
+  onDeleteProject,
+}: Props) {
+  const theme = useMantineTheme();
+  const [pages, setPages] = useState<PageResponse[]>([]);
+  const [pagesLoading] = useState(false);
+  const [menuOpened, setMenuOpened] = useState(false);
+  const [pagesOpened, setPagesOpened] = useState(false);
+  const [settingsOpened, setSettingsOpened] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const company = usePropelAuthStore((state) => state.activeCompany);
+  const isDexlaAdmin = usePropelAuthStore((state) => state.isDexlaAdmin);
+
+  const handleMenuOpen = () => {
+    setMenuOpened(true);
+  };
+
+  const handleMenuClose = () => {
+    setMenuOpened(false);
+    invalidate();
+  };
+
+  const {
+    data: pageListQuery,
+    isLoading: dataIsFetching,
+    isError,
+    invalidate,
+  } = usePageListQuery(projectId, undefined, menuOpened);
+
+  useEffect(() => {
+    if (!dataIsFetching && !isError && pageListQuery) {
+      setPages(pageListQuery.results || []);
+    }
+  }, [pageListQuery, dataIsFetching, isError]);
+
+  const deleteProjectFn = async () => {
+    setIsLoading(true);
+    await deleteProject(projectId);
+    onDeleteProject && onDeleteProject(projectId);
+    setPages([]);
+  };
+
+  const isDarkTheme = theme.colorScheme === "dark";
+
+  return (
+    <Menu
+      width={250}
+      withArrow
+      offset={20}
+      onOpen={handleMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <Menu.Target>
+        <UnstyledButton
+          sx={{
+            borderRadius: theme.radius.sm,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            ...buttonHoverStyles(theme),
+          }}
+        >
+          {isLoading ? (
+            <Loader size="sm" />
+          ) : (
+            <IconDots size={LARGE_ICON_SIZE} color={theme.colors.teal[5]} />
+          )}
+        </UnstyledButton>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Label>Project</Menu.Label>
+        <NavLink
+          label="Pages"
+          icon={<IconFileAnalytics size={ICON_SIZE} />}
+          rightSection={
+            pagesOpened ? (
+              <IconChevronDown size={ICON_SIZE} />
+            ) : (
+              <IconChevronRight size={ICON_SIZE} />
+            )
+          }
+          onClick={() => setPagesOpened((isOpen) => !isOpen)}
+        />
+        <Collapse in={pagesOpened}>
+          {pages.map((page) => {
+            return (
+              <Skeleton key={page.id} visible={pagesLoading}>
+                <Box ml={10}>
+                  <Menu.Item
+                    icon={
+                      page.isHome ? (
+                        <IconHome size={ICON_SIZE} />
+                      ) : (
+                        <IconFileAnalytics size={ICON_SIZE} />
+                      )
+                    }
+                    onClick={() => goToEditor(projectId, page.id)}
+                  >
+                    {page.title}
+                  </Menu.Item>
+                </Box>
+              </Skeleton>
+            );
+          })}
+        </Collapse>
+        <NavLink
+          label="Settings"
+          icon={<IconSettings size={ICON_SIZE} />}
+          rightSection={
+            settingsOpened ? (
+              <IconChevronDown size={ICON_SIZE} />
+            ) : (
+              <IconChevronRight size={ICON_SIZE} />
+            )
+          }
+          onClick={() => setSettingsOpened((isOpen) => !isOpen)}
+        />
+        <Collapse in={settingsOpened}>
+          <Box ml={10}>
+            <Menu.Item
+              icon={<Icon name="IconSettings2" size={ICON_SIZE} />}
+              component={Link}
+              href={`/projects/${projectId}/settings?name=${projectFriendlyName}`}
+            >
+              General
+            </Menu.Item>
+            <Menu.Item
+              icon={<Icon name="IconDatabase" size={ICON_SIZE} />}
+              component={Link}
+              href={`/projects/${projectId}/settings/datasources?name=${projectFriendlyName}`}
+            >
+              Datasource
+            </Menu.Item>
+            <Menu.Item
+              icon={<Icon name="IconWorldWww" size={ICON_SIZE} />}
+              component={Link}
+              href={`/projects/${projectId}/settings/domain?name=${projectFriendlyName}`}
+            >
+              Domain
+            </Menu.Item>
+          </Box>
+        </Collapse>
+        {isDexlaAdmin && (
+          <Menu.Item
+            icon={<Icon name="IconRefresh" />}
+            color={isDarkTheme ? "teal" : "indigo"}
+            component={Link}
+            href={`/projects/new?company=${company.orgId}&projectId=${projectId}&step=2`}
+          >
+            Regenerate Pages
+          </Menu.Item>
+        )}
+        {onDeleteProject && (
+          <Menu.Item
+            icon={<Icon name={ICON_DELETE} />}
+            color="red"
+            onClick={deleteProjectFn}
+          >
+            Delete
+          </Menu.Item>
+        )}
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
