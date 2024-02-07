fc17ce68230cd5c5717a8d2608a46fc597609d9d
Tom McDonough <tom@dexla.ai>
2024-02-07 14:10:56 -0800

    CU-86drfuahj - Added UI for page actions

---
+++ components/pages/PageActions.tsx
@@ -0,0 +1,16 @@
+import { Text } from "@mantine/core";
+
+type Props = {};
+
+export default function PageActions({}: Props) {
+  return (
+    <>
+      <Text size="sm">
+        Lets load triggers and actions here using the exact same component.
+      </Text>
+      <Text size="sm">
+        The triggers are the different part here which relate to a page loading.
+      </Text>
+    </>
+  );
+}
