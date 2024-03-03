84cb69efe9077449e5d94c2ece55a8a8c2899f48
Tom McDonough <tom@dexla.ai>
2024-03-03 18:07:12 +0000

    Added refetch swagger updates

--- components/datasources/SwaggerURLInputRevised.tsx
+++ components/datasources/SwaggerURLInputRevised.tsx
@@ -1,21 +1,41 @@
+import { useDataSourceEndpoints } from "@/hooks/reactQuery/useDataSourceEndpoints";
+import { useDataSources } from "@/hooks/reactQuery/useDataSources";
 import { getSwagger } from "@/requests/datasources/queries";
 import { useEditorStore } from "@/stores/editor";
-import { Flex, Loader, TextInput, TextInputProps } from "@mantine/core";
+import {
+  Flex,
+  Loader,
+  Stack,
+  Text,
+  TextInput,
+  TextInputProps,
+} from "@mantine/core";
 import { useState } from "react";
 import { ActionIconDefault } from "../ActionIconDefault";
 
 type Props = {
   datasourceId: string;
+  updated: number;
 } & TextInputProps;
 
-export const SwaggerURLInputRevised = ({ datasourceId, ...props }: Props) => {
+export const SwaggerURLInputRevised = ({
+  datasourceId,
+  updated,
+  ...props
+}: Props) => {
   const projectId = useEditorStore((state) => state.currentProjectId) as string;
   const [isLoading, setIsLoading] = useState(false);
+  const lastUpdated = new Date(updated).toLocaleString();
+  const { invalidate: invalidateDataSourceEndpoints } =
+    useDataSourceEndpoints(projectId);
+  const { invalidate: invalidateDataSources } = useDataSources(projectId);
 
   const refetchSwagger = async () => {
     try {
       setIsLoading(true);
       await getSwagger(projectId, datasourceId, props.value as string);
+      invalidateDataSources();
+      invalidateDataSourceEndpoints();
     } catch (e) {
       console.error(e);
     } finally {
@@ -24,20 +44,28 @@ export const SwaggerURLInputRevised = ({ datasourceId, ...props }: Props) => {
   };
 
   return (
-    <Flex align="end" gap="xs">
-      <TextInput
-        label="Swagger URL"
-        w="100%"
-        rightSection={isLoading && <Loader size="xs" />}
-        disabled={isLoading}
-        value={props.value}
-        {...props}
-      />
-      <ActionIconDefault
-        tooltip="Get Latest API"
-        iconName="IconRefresh"
-        onClick={refetchSwagger}
-      />
-    </Flex>
+    <Stack>
+      <Flex align="end" gap="xs">
+        <TextInput
+          label="Swagger URL"
+          w="100%"
+          rightSection={isLoading && <Loader size="xs" />}
+          disabled={isLoading}
+          value={props.value}
+          {...props}
+        />
+        <ActionIconDefault
+          tooltip="Get Latest API"
+          iconName="IconRefresh"
+          onClick={refetchSwagger}
+        />
+      </Flex>
+      <Flex gap="xs">
+        <Text size="xs">Last Updated:</Text>
+        <Text size="xs" color="dimmed">
+          {lastUpdated}
+        </Text>
+      </Flex>
+    </Stack>
   );
 };
