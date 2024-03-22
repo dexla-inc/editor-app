f43f56ca3e3c1ae04dfd9a8a1e1299f8384eb7bd
Tom McDonough <tom@dexla.ai>
2024-03-22 19:01:41 +0000

    Fixed children not getting mapped

--- utils/api.ts
+++ utils/api.ts
@@ -60,9 +60,9 @@ async function doFetch<Type>({
         json = await response.json();
       } catch (error) {
         if (error instanceof SyntaxError) {
-          console.log("There was a SyntaxError", error);
+          console.error("There was a SyntaxError", error);
         } else {
-          console.log("There was an error", error);
+          console.error("There was an error", error);
         }
       }
 
