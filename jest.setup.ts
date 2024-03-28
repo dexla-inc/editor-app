0158e8b08234435b0c5e308a1e9da12a7f946930
marcelo <142227648+marcelo-dexla@users.noreply.github.com>
2024-03-28 18:46:28 -0300

    fixing test environment (#846)
    * fixing test environment
    
    * testing environment working

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,18 @@
 import "@testing-library/jest-dom";
+
+global.fetch = require("isomorphic-fetch");
+
+jest.mock("nanoid");
+
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      // Mock properties here based on your test needs
+      path: "/",
+      asPath: "/",
+      query: {},
+      push: jest.fn(),
+      // Add any other router properties or methods your component uses
+    };
+  },
+}));
