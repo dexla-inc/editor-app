7c98f07d649ca14a561ad2720d1330a7cd95d335
Tom McDonough <tom@dexla.ai>
2024-04-08 09:50:43 +0100

    Add EditableComponent log for beta

--- components/EditableComponent.tsx
+++ components/EditableComponent.tsx
@@ -33,6 +33,7 @@ export const EditableComponent = ({
   selectedByOther,
   shareableContent,
 }: PropsWithChildren<Props>) => {
+  console.log("EditableComponent");
   const isPreviewMode = useEditorTreeStore((state) => state.isPreviewMode);
   const isLive = useEditorTreeStore((state) => state.isLive);
   const isEditorMode = !isPreviewMode && !isLive;
