e0eeaee11f62cb8c43dcec15cd6aefa559dd12e5
Tom McDonough <tom@dexla.ai>
2024-04-12 09:25:44 +0100

    Added sizing display size on SizeSelector

--- components/SizeSelector.tsx
+++ components/SizeSelector.tsx
@@ -1,26 +1,72 @@
-import { Select, SelectItem, SelectProps } from "@mantine/core";
+import { gapSizes, inputSizes } from "@/utils/defaultSizes";
+import {
+  Box,
+  MantineSize,
+  Select,
+  SelectItem,
+  SelectProps,
+  Text,
+} from "@mantine/core";
+
+interface CustomSelectItemProps {
+  label: string;
+  description: string;
+}
+
+const CustomSelectItem: React.FC<CustomSelectItemProps> = ({
+  label,
+  description,
+  ...others
+}) => (
+  <Box {...others}>
+    <Text>{label}</Text>
+    <Text size="xs" color="#ddd">
+      {description}
+    </Text>
+  </Box>
+);
 
 type Props = {
   showNone?: boolean;
   showFullscreen?: boolean;
   data?: Array<{ label: string; value: string }>;
+  sizing?: Record<MantineSize, string>;
 } & Omit<SelectProps, "data">;
 
 export const SizeSelector = ({
   showNone = true,
   showFullscreen = false,
   label = "Size",
+  sizing,
   ...props
 }: Props) => {
-  const defaultData = [
-    showNone && { label: "None", value: "0" },
-    { label: "Extra Small", value: "xs" },
-    { label: "Small", value: "sm" },
-    { label: "Medium", value: "md" },
-    { label: "Large", value: "lg" },
-    { label: "Extra Large", value: "xl" },
-    showFullscreen && { label: "Fullscreen", value: "fullscreen" },
-  ].filter(Boolean) as SelectItem[];
-  const data = ("data" in props ? props.data : defaultData) as SelectItem[];
-  return <Select label={label} size="xs" {...props} data={data} />;
+  const defaultData = [];
+
+  if (showNone) {
+    defaultData.push({ label: "None", value: "0", description: "0px" });
+  }
+
+  if (sizing) {
+    Object.entries(sizing).forEach(([value, description]) => {
+      defaultData.push({ label: value.toUpperCase(), value, description });
+    });
+  }
+
+  if (showFullscreen) {
+    defaultData.push({
+      label: "Fullscreen",
+      value: "fullscreen",
+      description: "Full screen",
+    });
+  }
+
+  return (
+    <Select
+      label={label}
+      size="xs"
+      {...props}
+      data={defaultData}
+      itemComponent={CustomSelectItem}
+    />
+  );
 };
