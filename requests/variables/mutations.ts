f0a592592f17e2b7909e6ad40eca5db9f0ba9507
Tom McDonough <tom@dexla.ai>
2024-04-16 17:48:40 +0100

    Added evict cache for variables

--- requests/variables/mutations.ts
+++ requests/variables/mutations.ts
@@ -2,6 +2,7 @@ import { SuccessResponse } from "@/requests/types";
 import { VariableResponse } from "@/requests/variables/types";
 import { useVariableStore } from "@/stores/variables";
 import { del, post, put } from "@/utils/api";
+import { evictCache } from "@/requests/cache/queries-noauth";
 
 const setVariable = useVariableStore.getState().setVariable;
 const deleteVariableFromStore = useVariableStore.getState().deleteVariable;
@@ -18,6 +19,7 @@ export const createVariable = async (
   )) as VariableResponse;
 
   setVariable({ id: response.id, ...params });
+  await evictCache(url);
   return response;
 };
 
@@ -33,7 +35,7 @@ export const updateVariable = async (
     params,
   )) as VariableResponse;
   setVariable({ id: response.id, value: params.defaultValue, ...params });
-
+  await evictCache(url);
   return response;
 };
 
@@ -41,6 +43,6 @@ export const deleteVariable = async (projectId: string, id: string) => {
   let url = `/projects/${projectId}/variables/${id}`;
   deleteVariableFromStore(id);
   const response = (await del<SuccessResponse>(url)) as SuccessResponse;
-
+  await evictCache(url);
   return response;
 };
