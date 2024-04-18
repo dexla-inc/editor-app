477d546c6e3e3b3ade995fb98869250aa066fdd4
Tom McDonough <tom@dexla.ai>
2024-04-18 19:23:14 +0100

    UI styling for Custom JavaScript

--- components/actions/CustomJavascriptActionForm.tsx
+++ components/actions/CustomJavascriptActionForm.tsx
@@ -4,6 +4,7 @@ import Editor from "@monaco-editor/react";
 import { IconPlayerPlayFilled } from "@tabler/icons-react";
 import { useState } from "react";
 import { transpile } from "typescript";
+import { MonacoEditorJson } from "../MonacoEditorJson";
 
 type Props = ActionFormProps<Omit<CustomJavascriptAction, "name">>;
 
@@ -17,42 +18,17 @@ export const CustomJavascriptActionForm = ({ form }: Props) => {
 
   return (
     <Stack spacing="xs">
-      <Flex w="100%" justify="space-between" align="center">
-        <Text size="sm" weight={500}>
-          Custom Javascript
-        </Text>
-        <Button
-          variant="default"
-          sx={{ marginRight: 0 }}
-          size="xs"
-          leftIcon={<IconPlayerPlayFilled size={8} />}
-          type="button"
-          onClick={onClickRunCode}
-        >
-          Run Code
-        </Button>
-      </Flex>
-      <Card style={{ overflow: "visible" }} withBorder radius="sm" px={0}>
-        <Editor
-          width="100%"
-          height="150px"
-          defaultLanguage="typescript"
-          defaultValue="// write your code here"
-          {...form.getInputProps("code")}
-          options={{
-            automaticLayout: true,
-            minimap: { enabled: false },
-            contextmenu: false,
-            language: {
-              javascript: {
-                globalKnownSymbols: {
-                  data: true,
-                },
-              },
-            },
-          }}
-        />
-      </Card>
+      <MonacoEditorJson height="250px" {...form.getInputProps("body")} />
+      <Button
+        variant="default"
+        sx={{ marginRight: 0 }}
+        size="xs"
+        leftIcon={<IconPlayerPlayFilled size={8} />}
+        type="button"
+        onClick={onClickRunCode}
+      >
+        Run Code
+      </Button>
 
       {codeResult && (
         <div style={{ height: 200, width: "100%", border: "1px solid #000" }}>
