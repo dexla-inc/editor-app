9b55fa5aa1ae9469118bac9423f484b0ebc0bde6
Tom McDonough <tom@dexla.ai>
2024-05-06 20:22:40 +0100

    Update OpenLogicFlowsButton.tsx

--- components/editor/OpenLogicFlowsButton.tsx
+++ components/editor/OpenLogicFlowsButton.tsx
@@ -1,23 +1,14 @@
 import { ActionIconDefault } from "@/components/ActionIconDefault";
 import { useLogicFlows } from "@/hooks/logic-flow/useLogicFlows";
-import { useEditorTreeStore } from "@/stores/editorTree";
 
 const OpenLogicFlowsButton = () => {
   const { openLogicFlowsModal } = useLogicFlows();
-  const setSelectedComponentIds = useEditorTreeStore(
-    (state) => state.setSelectedComponentIds,
-  );
-
-  const handleClick = () => {
-    setSelectedComponentIds(() => []);
-    openLogicFlowsModal();
-  };
 
   return (
     <ActionIconDefault
       iconName="IconGitBranch"
       tooltip="Logic Flows"
-      onClick={handleClick}
+      onClick={openLogicFlowsModal}
     />
   );
 };
