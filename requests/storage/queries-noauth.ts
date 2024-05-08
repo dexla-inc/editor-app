559e6be4575fc779b0ab65e4cececf3e57061dc5
Tom McDonough <tom@dexla.ai>
2024-05-08 18:23:52 +0100

    Added internal storage API endpoint to no auth api

--- requests/storage/queries-noauth.ts
+++ requests/storage/queries-noauth.ts
@@ -1,5 +1,32 @@
-import { UploadMultipleResponse } from "@/requests/storage/types";
+import {
+  UploadMultipleResponse,
+  UploadResponse,
+} from "@/requests/storage/types";
 import { getWithoutAuth } from "@/utils/apiNoAuth";
+import { FileWithPath } from "@mantine/dropzone";
+import { post } from "@/utils/api";
+
+export const uploadFile = async (
+  projectId: string,
+  file: File | File[] | FileWithPath | FileWithPath[],
+  isMultiple: boolean = false,
+) => {
+  let url = `/projects/${projectId}/storage/internal?isMultiple=${isMultiple}`;
+
+  const formData = new FormData();
+  if (Array.isArray(file)) file.forEach((f) => formData.append("file", f));
+  else formData.append("file", file);
+
+  const response = (await post<UploadMultipleResponse | UploadResponse>(
+    url,
+    formData,
+  )) as UploadMultipleResponse | UploadResponse;
+
+  // const cacheTag = getCacheTag(projectId);
+  // await evictCache(cacheTag);
+
+  return response;
+};
 
 export const getFile = async (projectId: string, name: string) => {
   let url = `/projects/${projectId}/storage?name=${name}?jsonReturnType=true`;
@@ -16,3 +43,5 @@ export const getAllFiles = async (projectId: string) => {
 
   return response;
 };
+
+//const getCacheTag = (projectId: string) => `/projects/${projectId}/storage`;
