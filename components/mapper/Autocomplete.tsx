0485581ebba1f6ee76da515e6ac6d749879ffb47
marcelo-dexla <marcelo@dexla.ai>
2024-05-10 14:36:09 -0300

    log

--- components/mapper/Autocomplete.tsx
+++ components/mapper/Autocomplete.tsx
@@ -102,7 +102,7 @@ const AutocompleteComponent = forwardRef(
       }
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [itemSubmitted]);
-    console.log({ data });
+
     return (
       <MantineAutocomplete
         ref={ref}
