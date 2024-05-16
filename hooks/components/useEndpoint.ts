cfeb51672242efdcef0ab3808deca3367d0a3620
Tom McDonough <tom@dexla.ai>
2024-05-16 14:39:28 +0100

    CU-86dqvpaw2 - Load Skeleton for all components that have data bound on load as default, include modifier to turn off.

--- hooks/components/useEndpoint.ts
+++ hooks/components/useEndpoint.ts
@@ -79,5 +79,9 @@ export const useEndpoint = ({
     },
   );
 
-  return { data, isLoading: isLoading && isEnabled };
+  return {
+    data,
+    isLoading: isLoading && isEnabled,
+    initiallyLoading: isLoading && isEnabled,
+  };
 };
