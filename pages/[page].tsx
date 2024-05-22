8f1071c86a49ef594c45d38e6f020dabe38c28d9
Tom McDonough <tom@dexla.ai>
2024-05-22 17:00:37 +0100

    Fixed deployments evicting the cache with the deployment url

--- pages/[page].tsx
+++ pages/[page].tsx
@@ -16,23 +16,8 @@ export const getServerSideProps = async ({
 }: GetServerSidePropsContext) => {
   const url = req.headers.host as string;
   const currentSlug = query.page as string;
-  console.log("url", url);
-  console.log("currentSlug", currentSlug);
-  const timer = Stopwatch.StartNew();
-  console.log(
-    "Before getDeploymentPage",
-    timer.getElapsedMilliseconds(),
-    url,
-    currentSlug,
-  );
+
   const deploymentPage = await getDeploymentPage(url, currentSlug);
-  console.log(
-    "After getDeploymentPage",
-    timer.getElapsedMilliseconds(),
-    url,
-    currentSlug,
-    `trackingId: ${deploymentPage.trackingId}`,
-  );
 
   if (!deploymentPage.projectId) {
     return {
@@ -48,34 +33,28 @@ export const getServerSideProps = async ({
   // Redirect to dexla page to say it hasn't been deployed
 
   const notFoundPageslug = deploymentPage.project.redirects?.notFoundPageId;
-  console.log("deploymentPage", deploymentPage);
+
   // Check if page exists
-  // if (!deploymentPage.id) {
-  //   console.log("404Redirect", notFoundPageslug);
-  //   return {
-  //     redirect: {
-  //       destination: notFoundPageslug
-  //         ? `/${notFoundPageslug}`
-  //         : "https://dexla.ai/404",
-  //       permanent: false,
-  //     },
-  //     props: {
-  //       dehydratedState: dehydrate(queryClient),
-  //       isLive: true,
-  //       project: deploymentPage.project,
-  //     },
-  //   };
-  // }
+  if (!deploymentPage.id) {
+    return {
+      redirect: {
+        destination: notFoundPageslug
+          ? `/${notFoundPageslug}`
+          : "https://dexla.ai/404",
+        permanent: false,
+      },
+      props: {
+        dehydratedState: dehydrate(queryClient),
+        isLive: true,
+        project: deploymentPage.project,
+      },
+    };
+  }
 
   // Check if user is logged in via cookies
   const cookie = req.cookies[deploymentPage.projectId];
   const isLoggedIn = checkRefreshTokenExists(cookie);
   const signInPageSlug = deploymentPage.project.redirects?.signInPageId;
-  console.log(
-    "After signInPageSlug",
-    timer.getElapsedMilliseconds(),
-    `trackingId: ${deploymentPage.trackingId}`,
-  );
 
   if (
     !isLoggedIn &&
@@ -94,11 +73,7 @@ export const getServerSideProps = async ({
       },
     };
   }
-  console.log(
-    "Before entering app",
-    timer.getElapsedMilliseconds(),
-    `trackingId: ${deploymentPage.trackingId}`,
-  );
+
   return {
     props: {
       dehydratedState: dehydrate(queryClient),
