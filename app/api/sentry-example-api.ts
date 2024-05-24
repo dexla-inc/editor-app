be34dd7b25560a6c94a1700ef3a0a3a61a56c4f3
marcelo-dexla <marcelo@dexla.ai>
2024-05-24 17:42:01 -0300

    moved route files to the new structure

--- app/api/sentry-example-api.ts
+++
@@ -1,5 +0,0 @@
-// A faulty API route to test Sentry's error monitoring
-export default function handler(_req: any, res: any) {
-  throw new Error("Sentry Example API Route Error");
-  return Response.json({ name: "John Doe" }, { status: 200 });
-}
