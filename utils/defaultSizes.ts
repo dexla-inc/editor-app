4e70d967c2ab00f95e4702e6e0b866b0199c07ea
Tom McDonough <tom@dexla.ai>
2024-05-28 10:50:05 +0100

    Added badge size

--- utils/defaultSizes.ts
+++ utils/defaultSizes.ts
@@ -1,16 +1,21 @@
 import { MantineSize } from "@mantine/core";
 
-type ModifierType = "input" | "gap" | "radius";
+type SizeDefinition = {
+  input: string;
+  gap: string;
+  radius: string;
+  badge?: string;
+};
 
 // Centralized size definitions
 const sizeDefinitions = {
   xxs: { input: "24px", gap: "4px", radius: "2px" },
-  xs: { input: "30px", gap: "8px", radius: "4px" },
-  sm: { input: "36px", gap: "12px", radius: "8px" },
-  md: { input: "40px", gap: "16px", radius: "10px" },
-  lg: { input: "48px", gap: "24px", radius: "16px" },
-  xl: { input: "60px", gap: "32px", radius: "32px" },
-} as Record<MantineSize, Record<ModifierType, string>>;
+  xs: { input: "30px", gap: "8px", radius: "4px", badge: "20px" },
+  sm: { input: "36px", gap: "12px", radius: "8px", badge: "24px" },
+  md: { input: "40px", gap: "16px", radius: "10px", badge: "28px" },
+  lg: { input: "48px", gap: "24px", radius: "16px", badge: "32px" },
+  xl: { input: "60px", gap: "32px", radius: "32px", badge: "36px" },
+} as Record<MantineSize, SizeDefinition>;
 
 // Utility function to create the records
 const createSizeRecord = (key: keyof (typeof sizeDefinitions)["xs"]) => {
@@ -19,14 +24,18 @@ const createSizeRecord = (key: keyof (typeof sizeDefinitions)["xs"]) => {
       size,
       values[key],
     ]),
-  ) as Record<MantineSize, string>;
+  ) as Record<MantineSize, string | undefined>;
 };
 
 export const inputSizes = createSizeRecord("input");
 export const gapSizes = createSizeRecord("gap");
 export const radiusSizes = createSizeRecord("radius");
+export const badgeSizes = createSizeRecord("badge");
 
-export const convertSizeToPx = (size: MantineSize, type: ModifierType) => {
+export const convertSizeToPx = (
+  size: MantineSize,
+  type: keyof SizeDefinition,
+) => {
   if (sizeDefinitions[size]) {
     return sizeDefinitions[size][type];
   }
