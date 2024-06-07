5aa5e7acfe56c84b22f96e2cd2e24d19a4bd5143
Tom McDonough <tom@dexla.ai>
2024-06-07 15:32:18 +0100

    Made charts clickable

--- components/mapper/charts/Chart.tsx
+++ components/mapper/charts/Chart.tsx
@@ -9,7 +9,8 @@ import { useEndpoint } from "@/hooks/components/useEndpoint";
 import groupBy from "lodash.groupby";
 import { Box, Skeleton } from "@mantine/core";
 import { omit } from "next/dist/shared/lib/router/utils/omit";
-import { useMemo } from "react";
+import { forwardRef, memo, useMemo } from "react";
+import { withComponentWrapper } from "@/hoc/withComponentWrapper";
 
 const ReactApexChart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
@@ -73,140 +74,148 @@ const baseOptions: ApexOptions = {
   dataLabels: { enabled: false },
 };
 
-export const Chart = ({ component, ...props }: Props) => {
-  const {
-    children,
-    series,
-    type,
-    options,
-    chartColors,
-    labelColor,
-    foreColor,
-    triggers,
-    dataType = "static",
-    ...componentProps
-  } = component.props as any;
+const ChartComponent = forwardRef(
+  ({ renderTree, shareableContent, component, ...props }: Props, ref) => {
+    const {
+      children,
+      series,
+      type,
+      options,
+      chartColors,
+      labelColor,
+      foreColor,
+      triggers,
+      dataType = "static",
+      ...componentProps
+    } = component.props as any;
 
-  const theme = useThemeStore((state) => state.theme);
-  const isPieOrRadial =
-    component?.name === "PieChart" || component?.name === "RadialChart";
+    const theme = useThemeStore((state) => state.theme);
+    const isPieOrRadial =
+      component?.name === "PieChart" || component?.name === "RadialChart";
 
-  const colors = useMemo(
-    () =>
-      chartColors?.map((color: any) => getColorFromTheme(theme, color)) ??
-      defaultColors(theme),
-    [chartColors, theme],
-  );
+    const colors = useMemo(
+      () =>
+        chartColors?.map((color: any) => getColorFromTheme(theme, color)) ??
+        defaultColors(theme),
+      [chartColors, theme],
+    );
 
-  const _labelColor = useMemo(
-    () => getChartColor(theme, labelColor, "SecondaryText.5"),
-    [theme, labelColor],
-  );
-  const _foreColor = useMemo(
-    () => getChartColor(theme, foreColor, "Secondary.5"),
-    [theme, foreColor],
-  );
+    const _labelColor = useMemo(
+      () => getChartColor(theme, labelColor, "SecondaryText.5"),
+      [theme, labelColor],
+    );
+    const _foreColor = useMemo(
+      () => getChartColor(theme, foreColor, "Secondary.5"),
+      [theme, foreColor],
+    );
 
-  const { data: response, isLoading } = useEndpoint({
-    componentId: component.id!,
-    onLoad: component.onLoad,
-    dataType,
-  });
+    const { data: response, isLoading } = useEndpoint({
+      componentId: component.id!,
+      onLoad: component.onLoad,
+      dataType,
+    });
 
-  let dataSeries = series;
-  let dynamicOptions = {};
+    let dataSeries = series;
+    let dynamicOptions = {};
 
-  if (dataType === "dynamic") {
-    dataSeries = [];
+    if (dataType === "dynamic") {
+      dataSeries = [];
 
-    if (response) {
-      // TODO: implement it back when the swagger is ready
-      // const grouped = groupBy(get(response, resultsKey, {}), dataLegendKey);
-      const grouped = groupBy(get(response, "data", []), "description");
-      if (isPieOrRadial) {
-        dataSeries = get(response, "data", []).map((i: any) => i.value);
-      } else {
-        dataSeries = Object.entries(grouped).map(([name, items]) => ({
-          name,
-          data: items.map((i: any) => i.value),
-        }));
-      }
+      if (response) {
+        // TODO: implement it back when the swagger is ready
+        // const grouped = groupBy(get(response, resultsKey, {}), dataLegendKey);
+        const grouped = groupBy(get(response, "data", []), "description");
+        if (isPieOrRadial) {
+          dataSeries = get(response, "data", []).map((i: any) => i.value);
+        } else {
+          dataSeries = Object.entries(grouped).map(([name, items]) => ({
+            name,
+            data: items.map((i: any) => i.value),
+          }));
+        }
 
-      const dataLabels = get(response, "data", []).map((i: any) => i.selector);
+        const dataLabels = get(response, "data", []).map(
+          (i: any) => i.selector,
+        );
 
-      dynamicOptions = {
-        ...(isPieOrRadial
-          ? { labels: dataLabels }
-          : {
-              xaxis: {
-                categories: dataLabels,
-                labels: {
-                  style: { colors: dataLabels?.map((_: any) => _foreColor) },
+        dynamicOptions = {
+          ...(isPieOrRadial
+            ? { labels: dataLabels }
+            : {
+                xaxis: {
+                  categories: dataLabels,
+                  labels: {
+                    style: { colors: dataLabels?.map((_: any) => _foreColor) },
+                  },
                 },
-              },
-            }),
-      };
+              }),
+        };
+      }
     }
-  }
 
-  const customOptions = useMemo(
-    () =>
-      merge(
-        {},
-        baseOptions,
-        {
-          colors,
-          chart: { ...baseOptions.chart, foreColor: _foreColor },
-          grid: {
-            ...baseOptions.grid,
-            borderColor: theme.fn.lighten(_foreColor, 0.2),
+    const customOptions = useMemo(
+      () =>
+        merge(
+          {},
+          baseOptions,
+          {
+            colors,
+            chart: { ...baseOptions.chart, foreColor: _foreColor },
+            grid: {
+              ...baseOptions.grid,
+              borderColor: theme.fn.lighten(_foreColor, 0.2),
+            },
+            markers: {
+              ...baseOptions.markers,
+              strokeColors: theme.colors.gray[0],
+            },
+            legend: {
+              ...baseOptions.legend,
+              show: type !== "radialBar" && dataSeries?.length > 0,
+              labels: { colors: _labelColor },
+            },
           },
-          markers: {
-            ...baseOptions.markers,
-            strokeColors: theme.colors.gray[0],
-          },
-          legend: {
-            ...baseOptions.legend,
-            show: type !== "radialBar" && dataSeries?.length > 0,
-            labels: { colors: _labelColor },
-          },
-        },
+          options,
+          dynamicOptions,
+        ),
+      [
+        colors,
+        _foreColor,
+        _labelColor,
         options,
         dynamicOptions,
-      ),
-    [
-      colors,
-      _foreColor,
-      _labelColor,
-      options,
-      dynamicOptions,
-      theme,
-      type,
-      dataSeries,
-    ],
-  );
+        theme,
+        type,
+        dataSeries,
+      ],
+    );
 
-  Object.assign(customOptions, dynamicOptions);
+    Object.assign(customOptions, dynamicOptions);
 
-  return (
-    <Skeleton visible={isLoading} id={component.id}>
-      <Box
-        component={ReactApexChart}
-        {...omit(props, ["id"])}
-        {...componentProps}
-        {...triggers}
-        series={dataSeries}
-        style={{
-          ...(props.style ?? {}),
-          textAlign: "center",
-          padding: 0,
-          color: theme.colors.gray[8],
-        }}
-        type={type}
-        options={customOptions}
-        width={componentProps.style?.width ?? "100%"}
-        height={componentProps.style?.height ?? 320}
-      />
-    </Skeleton>
-  );
-};
+    return (
+      <Skeleton visible={isLoading} id={component.id}>
+        <Box
+          component={ReactApexChart}
+          {...omit(props, ["id"])}
+          {...componentProps}
+          {...triggers}
+          series={dataSeries}
+          style={{
+            ...(props.style ?? {}),
+            textAlign: "center",
+            padding: 0,
+            color: theme.colors.gray[8],
+          }}
+          type={type}
+          options={customOptions}
+          width={componentProps.style?.width ?? "100%"}
+          height={componentProps.style?.height ?? 320}
+        />
+      </Skeleton>
+    );
+  },
+);
+
+ChartComponent.displayName = "Chart";
+
+export const Chart = memo(withComponentWrapper<Props>(ChartComponent));
