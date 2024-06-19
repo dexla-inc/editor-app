a63f1cd64855281b1a59c047968c82b7feb59153
Tom McDonough <tom@dexla.ai>
2024-06-19 15:24:03 +0100

    Removed sidebar section from RSS Feed

--- components/navbar/apps/EndpointSetup.tsx
+++ components/navbar/apps/EndpointSetup.tsx
@@ -1,6 +1,5 @@
 import { EndpointSelect } from "@/components/EndpointSelect";
 import { SegmentedControlYesNo } from "@/components/SegmentedControlYesNo";
-import SidebarSection from "@/components/SidebarSection";
 import { Endpoint } from "@/requests/datasources/types";
 import { AUTOCOMPLETE_OFF_PROPS, safeJsonParse } from "@/utils/common";
 import { DEFAULT_STALE_TIME } from "@/utils/config";
@@ -48,149 +47,131 @@ export const EndpointSetup = ({
 
   return (
     <>
-      <SidebarSection
-        id="data"
-        noPadding={true}
-        initiallyOpened={initiallyOpened}
-        label="Load Data"
-        icon="IconDatabase"
-        onClick={(id: string, opened: boolean) =>
-          id === "data" && setInitiallyOpened(opened)
-        }
-      >
-        <EndpointSelect
-          {...form.getInputProps(`apps.${index}.configuration.endpointId`)}
-          isOnLoad
-          onChange={async (selected) => {
-            form.setFieldValue(
-              `apps.${index}.configuration.endpointId`,
-              selected,
-            );
-            setSelectedEndpoint(
-              endpoints?.find((e) => e.id === selected) as Endpoint,
-            );
-          }}
-        />
-        {form.values.apps &&
-          form.values.apps[index].configuration.endpointId && (
-            <>
-              <Flex align="end" gap="xs" justify="space-between">
-                <SegmentedControlYesNo
-                  label="Cache Request"
-                  value={form.values.apps[index].configuration.staleTime !== 0}
-                  onChange={(value) => {
-                    form.setFieldValue(
-                      `apps.${index}.configuration.staleTime`,
-                      value === false ? 0 : DEFAULT_STALE_TIME,
-                    );
-                  }}
-                />
-                <TextInput
-                  disabled={
-                    form.values.apps[index].configuration.staleTime === 0
-                  }
-                  mt={8}
-                  w={80}
-                  {...form.getInputProps(
+      <EndpointSelect
+        {...form.getInputProps(`apps.${index}.configuration.endpointId`)}
+        isOnLoad
+        onChange={async (selected) => {
+          form.setFieldValue(
+            `apps.${index}.configuration.endpointId`,
+            selected,
+          );
+          setSelectedEndpoint(
+            endpoints?.find((e) => e.id === selected) as Endpoint,
+          );
+        }}
+      />
+      {form.values.apps && form.values.apps[index].configuration.endpointId && (
+        <>
+          <Flex align="end" gap="xs" justify="space-between">
+            <SegmentedControlYesNo
+              label="Cache Request"
+              value={form.values.apps[index].configuration.staleTime !== 0}
+              onChange={(value) => {
+                form.setFieldValue(
+                  `apps.${index}.configuration.staleTime`,
+                  value === false ? 0 : DEFAULT_STALE_TIME,
+                );
+              }}
+            />
+            <TextInput
+              disabled={form.values.apps[index].configuration.staleTime === 0}
+              mt={8}
+              w={80}
+              {...form.getInputProps(`apps.${index}.configuration.staleTime`)}
+              onChange={(e) => {
+                form.setFieldValue(
+                  `apps.${index}.configuration.staleTime`,
+                  e.target.value !== ""
+                    ? Number(e.target.value)
+                    : e.target.value,
+                );
+              }}
+              onBlur={(e) => {
+                if (e.target.value === "") {
+                  form.setFieldValue(
                     `apps.${index}.configuration.staleTime`,
-                  )}
-                  onChange={(e) => {
-                    form.setFieldValue(
-                      `apps.${index}.configuration.staleTime`,
-                      e.target.value !== ""
-                        ? Number(e.target.value)
-                        : e.target.value,
-                    );
-                  }}
-                  onBlur={(e) => {
-                    if (e.target.value === "") {
-                      form.setFieldValue(
-                        `apps.${index}.configuration.staleTime`,
-                        0,
-                      );
-                    }
-                  }}
-                  styles={{ rightSection: { right: "0.25rem" } }}
-                  rightSection={
-                    <Text size="xs" color="dimmed">
-                      mins
-                    </Text>
-                  }
-                  {...AUTOCOMPLETE_OFF_PROPS}
-                />
-              </Flex>
-              {!Array.isArray(exampleResponse) && (
-                <Select
-                  clearable
-                  label="Results key"
-                  searchable
-                  placeholder="results"
-                  data={resultsKeysList}
-                  {...form.getInputProps(
-                    `apps.${index}.configuration.resultsKey`,
-                  )}
-                />
-              )}
-
-              <Divider mt="md" />
-
-              <Stack spacing={2}>
-                {[
-                  {
-                    title: "Headers",
-                    type: "header" as ApiType,
-                    items: selectedEndpoint?.headers,
-                  },
-                  {
-                    title: "Request Body",
-                    type: "body" as ApiType,
-                    items: selectedEndpoint?.requestBody,
-                  },
-                  {
-                    title: "Query Strings",
-                    type: "parameter" as ApiType,
-                    items: selectedEndpoint?.parameters,
-                  },
-                ].map(({ title, type, items }) => (
-                  <React.Fragment key={title}>
-                    {items && items.length > 0 && (
-                      <Title order={5} mt="md">
-                        {title}
-                      </Title>
-                    )}
-                    {items &&
-                      items.map((param) => {
-                        let additionalProps = {};
-
-                        const field = `apps.${index}.configuration.binds.${type}.${param.name}`;
-
-                        return (
-                          <Stack key={param.name}>
-                            <BindingField
-                              size="xs"
-                              label={param.name}
-                              description={`${
-                                // @ts-ignore
-                                param.location ? `${param.location} - ` : ""
-                              }${param.type}`}
-                              type={param.type}
-                              fieldType="Text"
-                              {...(param.name !== "Authorization"
-                                ? // @ts-ignore
-                                  { required: param.required }
-                                : {})}
-                              {...additionalProps}
-                              {...form.getInputProps(field)}
-                            />
-                          </Stack>
-                        );
-                      })}
-                  </React.Fragment>
-                ))}
-              </Stack>
-            </>
+                    0,
+                  );
+                }
+              }}
+              styles={{ rightSection: { right: "0.25rem" } }}
+              rightSection={
+                <Text size="xs" color="dimmed">
+                  mins
+                </Text>
+              }
+              {...AUTOCOMPLETE_OFF_PROPS}
+            />
+          </Flex>
+          {!Array.isArray(exampleResponse) && (
+            <Select
+              clearable
+              label="Results key"
+              searchable
+              placeholder="results"
+              data={resultsKeysList}
+              {...form.getInputProps(`apps.${index}.configuration.resultsKey`)}
+            />
           )}
-      </SidebarSection>
+
+          <Divider mt="md" />
+
+          <Stack spacing={2}>
+            {[
+              {
+                title: "Headers",
+                type: "header" as ApiType,
+                items: selectedEndpoint?.headers,
+              },
+              {
+                title: "Request Body",
+                type: "body" as ApiType,
+                items: selectedEndpoint?.requestBody,
+              },
+              {
+                title: "Query Strings",
+                type: "parameter" as ApiType,
+                items: selectedEndpoint?.parameters,
+              },
+            ].map(({ title, type, items }) => (
+              <React.Fragment key={title}>
+                {items && items.length > 0 && (
+                  <Title order={5} mt="md">
+                    {title}
+                  </Title>
+                )}
+                {items &&
+                  items.map((param) => {
+                    let additionalProps = {};
+
+                    const field = `apps.${index}.configuration.binds.${type}.${param.name}`;
+
+                    return (
+                      <Stack key={param.name}>
+                        <BindingField
+                          size="xs"
+                          label={param.name}
+                          description={`${
+                            // @ts-ignore
+                            param.location ? `${param.location} - ` : ""
+                          }${param.type}`}
+                          type={param.type}
+                          fieldType="Text"
+                          {...(param.name !== "Authorization"
+                            ? // @ts-ignore
+                              { required: param.required }
+                            : {})}
+                          {...additionalProps}
+                          {...form.getInputProps(field)}
+                        />
+                      </Stack>
+                    );
+                  })}
+              </React.Fragment>
+            ))}
+          </Stack>
+        </>
+      )}
     </>
   );
 };
