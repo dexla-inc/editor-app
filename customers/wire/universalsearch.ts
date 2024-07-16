808a32249b6114461e2a42acec7c0751af8cd9f4
Tom McDonough <tom@dexla.ai>
2024-07-16 17:21:20 +0100

    Added universal search function for williams international

---
+++ customers/wire/universalsearch.ts
@@ -0,0 +1,44 @@
+// @ts-ignore
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+// @ts-ignore
+Deno.serve(async (req: Request) => {
+  const supabaseClient = createClient(
+    // @ts-ignore
+    Deno.env.get("SUPABASE_URL") ?? "",
+    // @ts-ignore
+    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    {
+      global: { headers: { Authorization: req.headers.get("Authorization")! } },
+    },
+  );
+
+  const url = new URL(req.url);
+  const query = url.searchParams.get("search");
+
+  if (!query) {
+    return new Response(
+      JSON.stringify({ error: "Search parameter is required" }),
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      },
+    );
+  }
+
+  // Database queries will have RLS policies enforced
+  const { data, error } = await supabaseClient.rpc("universal_search", {
+    query,
+  });
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+});
