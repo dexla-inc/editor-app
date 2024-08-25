94ffa28ec798f0f3c101a02f06fdf500a3825358
Williams Balogun <williams@dexla.ai>
2024-08-25 14:12:36 +0100

    added duplication feature to Williams intl plus small code fixes

--- customers/wire/fetch_tasks.ts
+++ customers/wire/fetch_tasks.ts
@@ -50,7 +50,12 @@ Deno.serve(async (req: Request) => {
     const limit = parseInt(p_limit, 10);
     const currentPage = parseInt(p_page, 10);
     const prevPage = currentPage > 1 ? currentPage - 1 : currentPage;
-    const nextPage = data.length === limit ? currentPage + 1 : currentPage;
+    const nextPage = data.totalCount === limit ? currentPage + 1 : currentPage;
+    const page_offset = (currentPage - 1) * limit;
+    const start = data.totalCount > 0 ? page_offset + 1 : 0;
+    const end = Math.min(start + limit - 1, data.totalCount);
+
+    const pageText = `${start} - ${end} of ${data.totalCount}`;
 
     // @ts-ignore
     response.pagination = {
@@ -58,6 +63,7 @@ Deno.serve(async (req: Request) => {
       currentPage,
       nextPage,
       totalPages: data.totalPages,
+      pageText,
     };
   }
 
