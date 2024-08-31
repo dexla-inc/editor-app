626d8bedc86fe19092e4d03824e7b8307204f133
Williams Balogun <williams@dexla.ai>
2024-08-31 16:17:16 +0100

    updated CodeEmbed mapper file

--- components/mapper/CodeEmbed.tsx
+++ components/mapper/CodeEmbed.tsx
@@ -2,7 +2,7 @@ import { withComponentWrapper } from "@/hoc/withComponentWrapper";
 import { EditableComponentMapper } from "@/utils/editor";
 import { Box } from "@mantine/core";
 import { omit } from "next/dist/shared/lib/router/utils/omit";
-import { forwardRef, useEffect } from "react";
+import { forwardRef } from "react";
 
 type Props = EditableComponentMapper & {
   htmlCode: string;
@@ -12,20 +12,40 @@ type Props = EditableComponentMapper & {
 
 const CodeEmbedComponent = forwardRef<HTMLDivElement, Props>(
   ({ component, ...props }, ref) => {
-    const { htmlCode, cssCode, jsCode, triggers, ...componentProps } =
-      component.props ?? {};
-    useEffect(() => {
-      if (ref && "current" in ref && ref.current) {
-        ref.current.innerHTML = `
-          <style>${cssCode}</style>
+    const {
+      htmlCode: html,
+      cssCode: css,
+      jsCode: js,
+      triggers,
+      ...componentProps
+    } = component.props ?? {};
+    const {
+      htmlCode = html,
+      cssCode = css,
+      jsCode = js,
+    } = component.onLoad ?? {};
+    const uniqueClass = `code-embed-${component.id}`;
+
+    if (ref && "current" in ref && ref.current) {
+      // Function to prefix CSS selectors
+      const prefixCssSelectors = (css: string, prefix: string) => {
+        return css.replace(/([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/g, (match) => {
+          // Don't prefix @-rules
+          return match.startsWith("@") ? match : `${prefix} ${match}`;
+        });
+      };
+      const prefixedCss = prefixCssSelectors(cssCode, `.${uniqueClass}`);
+
+      ref.current.innerHTML = `
+          <style>${prefixedCss}</style>
           ${htmlCode}
           <script>${jsCode}</script>
         `;
-      }
-    }, [htmlCode, cssCode, jsCode, ref]);
+    }
 
     return (
       <Box
+        className={uniqueClass}
         ref={ref}
         {...omit(props, ["style"])}
         {...triggers}
