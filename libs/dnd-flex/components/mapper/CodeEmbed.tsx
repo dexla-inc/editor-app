cf1fa7ce6f97f2f48e16fb24c0a8c5dc0d4a934a
marcelo-dexla <marcelo@dexla.ai>
2024-10-04 12:41:54 -0300

    code embed attempt 5

--- libs/dnd-flex/components/mapper/CodeEmbed.tsx
+++ libs/dnd-flex/components/mapper/CodeEmbed.tsx
@@ -1,5 +1,5 @@
 import { withComponentWrapper } from "@/hoc/withComponentWrapper";
-// import { useCodeInjection } from "@/hooks/editor/useCodeInjection";
+import { useCodeInjection } from "@/hooks/editor/useCodeInjection";
 import { EditableComponentMapper } from "@/utils/editor";
 import { Box, BoxProps } from "@mantine/core";
 import { forwardRef, useRef } from "react";
@@ -7,13 +7,11 @@ import { forwardRef, useRef } from "react";
 type Props = EditableComponentMapper & BoxProps;
 
 const CodeEmbedComponent = forwardRef<HTMLIFrameElement, Props>(
-  ({ component, shareableContent, ...props }, ref) => {
+  ({ component, shareableContent, renderTree, ...props }, ref) => {
     const iframeRef = useRef<HTMLIFrameElement>(null);
-    // useCodeInjection(iframeRef, component, props);
+    useCodeInjection(iframeRef, component, props);
 
-    const { triggers, ...componentProps } = component.props ?? {};
-
-    return <div />;
+    return <Box component="iframe" ref={iframeRef} {...props} />;
   },
 );
 
