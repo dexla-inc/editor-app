ace2948831147b43b0e8240cacf6d89c5dec851c
marcelo-dexla <marcelo@dexla.ai>
2024-10-04 13:07:07 -0300

    code embed attempt 6

--- libs/dnd-flex/components/mapper/CodeEmbed.tsx
+++ libs/dnd-flex/components/mapper/CodeEmbed.tsx
@@ -2,18 +2,35 @@ import { withComponentWrapper } from "@/hoc/withComponentWrapper";
 import { useCodeInjection } from "@/hooks/editor/useCodeInjection";
 import { EditableComponentMapper } from "@/utils/editor";
 import { Box, BoxProps } from "@mantine/core";
-import { forwardRef, useRef } from "react";
+import { forwardRef, useRef, Suspense } from "react";
 
 type Props = EditableComponentMapper & BoxProps;
 
-const CodeEmbedComponent = forwardRef<HTMLIFrameElement, Props>(
-  ({ component, shareableContent, renderTree, ...props }, ref) => {
-    const iframeRef = useRef<HTMLIFrameElement>(null);
-    useCodeInjection(iframeRef, component, props);
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-    return <Box component="iframe" ref={iframeRef} {...props} />;
+const CodeEmbedComponent = forwardRef<HTMLIFrameElement, Props>(
+  (props, ref) => {
+    return (
+      <Suspense>
+        {delay(1000).then(() => (
+          <CodeEmbedComponentInner {...props} />
+        ))}
+      </Suspense>
+    );
   },
 );
+
+const CodeEmbedComponentInner = ({
+  component,
+  shareableContent,
+  renderTree,
+  ...props
+}: Props) => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  useCodeInjection(iframeRef, component, props);
+
+  return <iframe ref={iframeRef} {...props} />;
+};
 
 CodeEmbedComponent.displayName = "CodeEmbed";
 
