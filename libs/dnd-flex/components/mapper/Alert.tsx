debb9c29d6ede7a2f67e36f529b0d03d30184f58
Williams Balogun <williams@dexla.ai>
2024-10-08 11:47:18 +0100

    Fix Alert component styling and update useBindingPopover hook

--- libs/dnd-flex/components/mapper/Alert.tsx
+++ libs/dnd-flex/components/mapper/Alert.tsx
@@ -1,36 +1,37 @@
 import { Icon } from "@/components/Icon";
 import { withComponentWrapper } from "@/hoc/withComponentWrapper";
 import { useThemeStore } from "@/stores/theme";
+import { getColorValue } from "@/utils/branding";
 import { EditableComponentMapper } from "@/utils/editor";
 import { AlertProps, Alert as MantineAlert } from "@mantine/core";
-import get from "lodash.get";
 import { forwardRef, memo } from "react";
 
 type Props = EditableComponentMapper & Omit<AlertProps, "title">;
 
 const AlertComponent = forwardRef(
   ({ renderTree, shareableContent, component, ...props }: Props, ref) => {
-    const { icon, iconColor, triggers, ...componentProps } =
+    const { icon, iconColor, color, triggers, ...componentProps } =
       component.props as any;
     const { children: childrenValue } = component?.onLoad;
     const theme = useThemeStore((state) => state.theme);
-    const iconColorHex = get(theme.colors, iconColor);
+    const iconColorHex = getColorValue(theme, iconColor);
+    const colorHex = getColorValue(theme, color);
 
     return (
       <MantineAlert
         ref={ref}
         {...(icon && {
-          icon: (
-            <Icon
-              name={icon}
-              // @ts-ignore
-              color={iconColorHex}
-            />
-          ),
+          icon: <Icon name={icon} color={iconColorHex} />,
         })}
+        variant="light"
         {...props}
         {...componentProps}
         {...triggers}
+        styles={{
+          root: {
+            backgroundColor: colorHex,
+          },
+        }}
         style={{ ...props.style }}
       >
         {component.children && component.children.length > 0
