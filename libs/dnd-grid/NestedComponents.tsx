721b91b7072acb4e84a62c43b407550b7a20aaaf
marcelo-dexla <marcelo@dexla.ai>
2024-10-08 13:17:16 -0300

    fixed storybook

--- libs/dnd-grid/NestedComponents.tsx
+++ libs/dnd-grid/NestedComponents.tsx
@@ -1,17 +1,39 @@
-import { MantineProvider } from "@mantine/core";
+import { Box, MantineProvider } from "@mantine/core";
 import ComponentList from "@/libs/dnd-grid/components/ComponentList";
 import ComponentToolbox from "@/libs/dnd-grid/components/ComponentToolbox";
 import ErrorBoundary from "@/libs/dnd-grid/components/ErrorBoundary";
 import DnDGrid from "@/libs/dnd-grid/DnDGrid";
+import { useState } from "react";
+import { createPortal } from "react-dom";
 
-export const NestedComponents = ({ components, iframeWindow }: any) => {
+export const NestedComponents = ({ components }: any) => {
+  const [contentRef, setContentRef] = useState<HTMLIFrameElement | null>(null);
+  const mountNode = contentRef?.contentWindow?.document.body;
+
   return (
     <ErrorBoundary>
       <MantineProvider>
         <div style={{ padding: "10px" }}>
           <ComponentToolbox />
           <ComponentList />
-          <DnDGrid components={components} />
+          <Box
+            id="iframe-canvas"
+            ref={setContentRef as any}
+            component="iframe"
+            style={{ width: "100%", height: "500px", border: "none" }}
+            allow="clipboard-read; clipboard-write"
+          >
+            {mountNode &&
+              createPortal(
+                <Box id="iframe-content">
+                  <DnDGrid
+                    components={components}
+                    iframeWindow={contentRef.contentWindow}
+                  />
+                </Box>,
+                mountNode,
+              )}
+          </Box>
         </div>
       </MantineProvider>
     </ErrorBoundary>
