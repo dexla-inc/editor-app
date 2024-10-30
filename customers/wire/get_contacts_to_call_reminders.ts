561f31f8bfe0f9936d4c138cacc0665b4f4fce79
Tom McDonough <tom@dexla.ai>
2024-10-30 12:44:38 +0000

    Changed get_contacts_to_call_reminders

--- customers/wire/get_contacts_to_call_reminders.ts
+++ customers/wire/get_contacts_to_call_reminders.ts
@@ -14,32 +14,19 @@ Deno.serve(async (req: Request) => {
   );
 
   const url = new URL(req.url);
-  const type = url.searchParams.get("type");
   const complete = url.searchParams.get("complete");
   const startDate = url.searchParams.get("start_date");
   const endDate = url.searchParams.get("end_date");
   const offset = parseInt(url.searchParams.get("offset") ?? "0");
-  const limit = parseInt(url.searchParams.get("limit") ?? "10");
-
-  if (!type) {
-    return new Response(
-      JSON.stringify({ error: "Type parameter is required" }),
-      {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      },
-    );
-  }
+  const limit = parseInt(url.searchParams.get("limit") ?? "50");
 
   const params: {
-    p_contact_type: string;
     p_is_completed?: boolean;
     p_start_date?: string;
     p_end_date?: string;
     p_offset: number;
     p_limit: number;
   } = {
-    p_contact_type: type,
     p_offset: offset,
     p_limit: limit,
   };
@@ -59,26 +46,34 @@ Deno.serve(async (req: Request) => {
     });
   }
 
-  const totalRecords = data[0]?.total_count ?? 0;
-  const results = data.map((item: any) => ({
-    task_id: item.task_id,
-    property_listing_id: item.property_listing_id,
-    contact_id: item.contact_id,
-    contact_name: item.contact_name,
-    contact_phone: item.contact_phone,
-    contact_email: item.contact_email,
-    task_datetime: item.task_datetime,
-    task_description: item.task_description,
-    task_is_completed: item.task_is_completed,
-  }));
+  function getGroupedContacts(data: any[]) {
+    const groupedResults = data.reduce(
+      (acc: { [key: string]: any[] }, item: any) => {
+        const { contact_type, ...rest } = item;
+        const contactType = contact_type;
+
+        if (!acc[contactType]) {
+          acc[contactType] = [];
+        }
+
+        acc[contactType].push(rest);
+
+        return acc;
+      },
+      {},
+    );
+
+    return groupedResults;
+  }
+
+  const results = getGroupedContacts(data);
 
   const response = {
     results,
     paging: {
-      totalRecords,
+      totalRecords: data.length,
       recordsPerPage: limit,
       page: Math.floor(offset / limit) + 1,
-      complete: complete || "false",
     },
   };
 
